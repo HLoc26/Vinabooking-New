38e5d0fe6633b7f1640fd2fec6a101a3b6ee8f23
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-08-25 00:34:28 +0700

    feat: update index.ts

--- API/user-service/src/index.ts
+++ API/user-service/src/index.ts
@@ -1,16 +1,13 @@
 import express from "express";
+import UserRouter from "./routes/user.routes.ts";
+import ErrorHandler from "./middlewares/ErrorHandler.ts";
 const app = express();
 app.use(express.json());
 
 // Base route: /users
+app.use("/", new UserRouter().router);
 
-app.get("/health", (req, res) => {
-	res.json({ service: "User Service", success: true });
-});
-
-app.get("/:id", (req, res) => {
-	res.json({ id: req.params.id, name: "Nguyen Van A" });
-});
+app.use(ErrorHandler.handle);
 
 const PORT = process.env.PORT || 3006;
 app.listen(PORT, () => console.log(`User Service running on port ${PORT}`));
