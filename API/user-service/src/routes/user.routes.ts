eaead178d3ab9b05b0a78830c8d8bd2814ffbf64
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-08-25 00:33:13 +0700

    feat: implement simple getUserById route

---
+++ API/user-service/src/routes/user.routes.ts
@@ -0,0 +1,28 @@
+import express, { type Router, type Request, type Response } from "express";
+import ResponseHelper from "../utils/ResponseHelper.ts";
+import UserController from "../controllers/UserController.ts";
+
+// Base route: /users
+
+class UserRouter {
+    public router: Router;
+    private userController: UserController;
+
+    constructor() {
+        this.router = express.Router();
+        this.userController = new UserController();
+        this.registerRoutes();
+    }
+
+    private registerRoutes(): void {
+        // health check
+        this.router.get("health", (_: Request, res: Response) => {
+            ResponseHelper.success(res, { service: "User Service", success: true });
+        });
+
+        // Get user by Id
+        this.router.get("/:id", (req: Request<{ id: string }>, res: Response) => this.userController.getUserById(req, res));
+    }
+}
+
+export default UserRouter;
