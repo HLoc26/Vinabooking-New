7d025eb11c37fc407d388956966e922a9d160e4c
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-08-25 14:53:10 +0700

    refactor(user): rename from user.routes.ts to UserRouter.ts for consistency

--- API/user-service/src/index.ts
+++ API/user-service/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import UserRouter from "./routes/user.routes.ts";
+import UserRouter from "./routes/UserRouter.ts";
 import ErrorHandler from "./middlewares/ErrorHandler.ts";
 const app = express();
 app.use(express.json());
