90e8d2a82e7541e6415e79137071d2bc2a2c7b65
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-08-25 14:51:07 +0700

    feat(auth): define sign up route

---
+++ API/auth-service/src/routes/UserRouter.ts
@@ -0,0 +1,32 @@
+import Router, { type Request, type Response } from "express";
+import AuthController from "../controllers/AuthController.ts";
+
+// Base route: /auth
+class AuthRouter {
+    public router = Router();
+    private authController = new AuthController();
+
+    constructor() {
+        this.registerRoutes();
+    }
+
+    private registerRoutes() {
+        this.router.get("/health", (_req: Request, res: Response) => {
+            res.json({ service: "Auth Service", success: true });
+        });
+
+        this.router.post("/sign-up", (req: Request, res: Response) => {
+            return this.authController.signUp(req, res);
+        });
+
+        // this.router.post("/sign-up/confirm");
+
+        // this.router.post("/sign-in");
+
+        // this.router.get("/refresh-token");
+
+        // this.router.post("/sign-out");
+    }
+}
+
+export default AuthRouter;
