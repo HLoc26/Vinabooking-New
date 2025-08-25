1b22ff8fbb41c25ed7566ac5830f9c3a8775c8fd
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-08-25 14:51:26 +0700

    feat(auth): update index.ts to use AuthRouter and ErrorHandler

--- API/auth-service/src/index.ts
+++ API/auth-service/src/index.ts
@@ -1,9 +1,25 @@
 import express from "express";
-import AuthRouter from "./routes/auth.routes.ts";
+import AuthRouter from "./routes/UserRouter.ts";
+import session from "express-session";
+
+import "dotenv";
+import ErrorHandler from "./middlewares/ErrorHandler.ts";
+
 const app = express();
 app.use(express.json());
 
+app.use(
+    session({
+        secret: process.env["SESSION_SECRET"] as string,
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false },
+    })
+);
+
 app.use("/", new AuthRouter().router);
 
-const PORT = process.env.PORT || 3002;
+app.use(ErrorHandler.handle);
+
+const PORT = process.env["PORT"] || 3002;
 app.listen(PORT, () => console.log(`Auth Service running on port ${PORT}`));
