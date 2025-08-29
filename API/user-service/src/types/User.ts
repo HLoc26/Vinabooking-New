732a994428e837ca23e595d24b712e384676f658
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-08-29 17:41:09 +0700

    feat(user): define types for User and Favourite

---
+++ API/user-service/src/types/User.ts
@@ -0,0 +1,30 @@
+import { type User as UserSchema } from "../../generated/prisma/index.js";
+
+import { type FavouriteListWithItems, type IFavouriteList } from "./Favourite.ts";
+
+export type UserWithFavourites = { favourites: FavouriteListWithItems[] } & UserSchema;
+
+// export enum EUserRole {
+//     traveler = "Traveler",
+//     accomm_owner = "AccommodationOwner",
+// }
+
+// Mimics enum
+export const EUserRole = {
+    TRAVELER: "Traveler",
+    ACCOMMODATION_OWNER: "AccommodationOwner",
+} as const;
+
+export type EUserRole = (typeof EUserRole)[keyof typeof EUserRole];
+
+export interface IUser {
+    id: string;
+    name: string;
+    phone: string;
+    role: string;
+
+    favouriteLists: Array<IFavouriteList>;
+
+    createdAt?: Date;
+    updatedAt?: Date;
+}
