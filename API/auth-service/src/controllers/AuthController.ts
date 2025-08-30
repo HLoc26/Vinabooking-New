dae3e628dd155e0100fab325338e4249a74f6118
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-08-31 00:25:13 +0700

    refactor(auth): update signUp method to use SignUpRequest and return structured SignUpResponse

--- API/auth-service/src/controllers/AuthController.ts
+++ API/auth-service/src/controllers/AuthController.ts
@@ -1,16 +1,20 @@
 import { type Response } from "express";
-import { type AuthRequestBody, type TypedRequest } from "../types/Request.ts";
+import { type SignUpRequest } from "../types/Request.ts";
 import AuthService from "../services/AuthService.ts";
 import ResponseHelper from "../utils/ResponseHelper.ts";
+import type { ApiResponse, SignUpResponse } from "../types/Response.ts";
+import IdentityProviderError from "../errors/IdentityProviderError.ts";
 
 class AuthController {
     private authService = new AuthService();
     constructor() {}
 
-    public async signUp(req: TypedRequest<AuthRequestBody>, res: Response) {
+    public async signUp(req: SignUpRequest, res: Response<ApiResponse<SignUpResponse>>) {
         const { username, password, email } = req.body;
 
         const response = await this.authService.signUp(username, password, email);
+
+        if (!response) throw new IdentityProviderError();
 
         return ResponseHelper.success(res, response);
     }
