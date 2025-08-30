aaaba96cce81ced0f4210a76afb120751f68ab70
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-08-31 00:36:24 +0700

    feat(redis): implement Redis singleton client for connection management

---
+++ API/user-service/src/clients/RedisSingleton.ts
@@ -0,0 +1,16 @@
+import { createClient, type RedisClientType } from "redis";
+
+class RedisClient {
+    private static instance: RedisClientType;
+
+    constructor() {}
+
+    public static async getInstance() {
+        if (!this.instance) {
+            this.instance = createClient();
+        }
+        return this.instance;
+    }
+}
+
+export default RedisClient;
