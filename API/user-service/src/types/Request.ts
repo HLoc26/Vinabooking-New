57fd9fb2f685e8845a150eef03a50880588be38d
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-08-31 22:53:00 +0700

    feat(user): update Request types for CacheUserRequest

--- API/user-service/src/types/Request.ts
+++ API/user-service/src/types/Request.ts
@@ -1,6 +1,6 @@
 import type { Request } from "express";
-import type { ApiResponse, UserResponse } from "./Response.ts";
+import type { ApiResponse, UserResponse, CacheUserResponse } from "./Response.ts";
 
 export type FindUserByIdRequest = Request<{ id: string }, ApiResponse<UserResponse>, unknown, { withFavourites?: string }>;
 
-export type CacheUserRequest = Request<unknown, ApiResponse<boolean>, { cognitoSub: string; email: string }, unknown>;
+export type CacheUserRequest = Request<unknown, ApiResponse<CacheUserResponse>, { cognitoSub: string; email: string }, unknown>;
