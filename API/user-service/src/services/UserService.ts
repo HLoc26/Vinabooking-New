cee72e7a178102895c1518601a89059d1f694c95
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-08-31 22:54:31 +0700

    feat(user): implement caching user data with Redis

--- API/user-service/src/services/UserService.ts
+++ API/user-service/src/services/UserService.ts
@@ -1,10 +1,13 @@
 import type { User as UserSchema } from "../../generated/prisma/index.js";
 import User from "../classes/User.ts";
+import RedisClient from "../clients/RedisSingleton.ts";
+import RedisClientError from "../errors/RedisClientError.ts";
 import UserRepository from "../repositories/UserRepository.ts";
 import type { UserWithFavourites } from "../types/User.ts";
 
 class UserService {
     private userRepository = new UserRepository();
+    private redisClient = RedisClient.getInstance();
 
     public async getUserById(id: string, withFavourites: boolean = false): Promise<User | null> {
         console.log("in service", withFavourites);
@@ -16,6 +19,15 @@ class UserService {
         const user = User.fromSchema(result);
 
         return user;
+    }
+
+    public async cacheUser(cognitoSub: string, email: string): Promise<boolean> {
+        const result: string | null = await this.redisClient.set(cognitoSub, email, { expiration: { type: "EX", value: 3600 } });
+
+        if (!result) {
+            throw new RedisClientError("Failed to save user to cache");
+        }
+        return true;
     }
 }
 
