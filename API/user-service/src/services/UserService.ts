44578747afec446226f802ccfc86aa29024f14f9
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-09-03 21:07:12 +0700

    refactor(user): streamline Redis client usage in UserService

--- API/user-service/src/services/UserService.ts
+++ API/user-service/src/services/UserService.ts
@@ -1,13 +1,12 @@
 import type { User as UserSchema } from "../../generated/prisma/index.js";
 import User from "../classes/User.ts";
-import RedisClient from "../clients/RedisSingleton.ts";
+import redisClient from "../clients/RedisSingleton.ts";
 import RedisClientError from "../errors/RedisClientError.ts";
 import UserRepository from "../repositories/UserRepository.ts";
 import type { UserWithFavourites } from "../types/User.ts";
 
 class UserService {
     private userRepository = new UserRepository();
-    private redisClient = RedisClient.getInstance();
 
     public async getUserById(id: string, withFavourites: boolean = false): Promise<User | null> {
         const result: UserWithFavourites | UserSchema | null = await this.userRepository.getUserById(id, withFavourites);
@@ -20,23 +19,19 @@ class UserService {
     }
 
     public async cacheUser(cognitoSub: string, email: string): Promise<boolean> {
-        this.redisClient.connect();
-        const result: string | null = await this.redisClient.set(cognitoSub, email);
+        const result: string | null = await redisClient.set(cognitoSub, email);
 
         if (!result) {
             throw new RedisClientError("Failed to save user to cache");
         }
-        this.redisClient.close();
         return true;
     }
 
     public async deleteCache(cognitoSub: string): Promise<boolean> {
-        this.redisClient.connect();
-        const result = await this.redisClient.del(cognitoSub);
+        const result = await redisClient.del(cognitoSub);
         if (!result) {
             throw new RedisClientError(`Failed to delete user ${cognitoSub}`);
         }
-        this.redisClient.close();
         return true;
     }
 }
