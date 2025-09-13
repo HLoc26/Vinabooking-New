4dca91fbcd090efa575a13bea43e199001b56e67
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-09-13 11:17:16 +0700

    feat(image): implement upload route

---
+++ API/image-service/src/routes/ImageRouter.ts
@@ -0,0 +1,29 @@
+import { Router, type Request, type Response } from "express";
+import ResponseHelper from "../utils/ResponseHelper.ts";
+import ImageController from "../controllers/ImageController.ts";
+import type { UploadRequest } from "../types/Request.ts";
+import UploadClient from "../clients/UploadClient.ts";
+
+// Base route: /images
+class ImageRouter {
+    public router = Router();
+    private imageController = new ImageController();
+    private uploadClient = UploadClient.getInstance();
+
+    constructor() {
+        this.registerRoutes();
+    }
+
+    private registerRoutes() {
+        this.router.get("/health", (_req: Request, res: Response) => {
+            return ResponseHelper.success(res, { service: "Image Service", success: true });
+        });
+
+        this.router.post("/:type/:id", this.uploadClient.array("files", 10), (req: Request, res: Response) => {
+            const uploadRequest = req as unknown as UploadRequest;
+            return this.imageController.upload(uploadRequest, res);
+        });
+    }
+}
+
+export default ImageRouter;
