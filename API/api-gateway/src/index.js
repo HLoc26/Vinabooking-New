bd6c89365ce6702bc47bd184514cfff760825ec3
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-09-13 10:26:20 +0700

    feat(api-gateway): add image-service endpoint

--- API/api-gateway/src/index.js
+++ API/api-gateway/src/index.js
@@ -22,5 +22,6 @@ app.use("/bookings", proxy(process.env.BOOKING_ENDPOINT));
 app.use("/reviews", proxy(process.env.REVIEW_ENDPOINT));
 app.use("/rooms", proxy(process.env.ROOM_ENDPOINT));
 app.use("/users", proxy(process.env.USER_ENDPOINT));
+app.use("/images", proxy(process.env.IMAGE_ENDPOINT));
 
 app.listen(3000, () => console.log("API Gateway running on port 3000"));
