3d501ba9695e1f6db5c4916324fddf488e94bbe4
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-09-13 11:19:08 +0700

    feat(image): use ImageRouter and ErrorHandler

--- API/image-service/src/index.ts
+++ API/image-service/src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
+import ImageRouter from "./routes/ImageRouter.ts";
+import ErrorHandler from "./middlewares/ErrorHandler.ts";
 const app = express();
 app.use(express.json());
 
 // Base route: /image
 
-app.get("/health", (_req, res) => {
-    res.json({ service: "Image Service", success: true });
-});
+app.use(new ImageRouter().router);
+
+app.use(ErrorHandler.handle);
 
 const PORT = process.env["PORT"] || 3007;
 app.listen(PORT, () => console.log(`Image Service running on port ${PORT}`));
