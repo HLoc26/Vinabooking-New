69b24474c3456d7bf2187b7dddf620a15a1913ea
sang-ute <nqs28012004@gmail.com>
2025-10-15 21:37:23 +0700

    feat(prisma): add migration for Booking table updates and new BookingDetail table

--- API/booking-service/prisma/migrations/20251015143413_2_migration/migration.sql
+++ API/booking-service/prisma/migrations/20251015143413_2_migration/migration.sql
@@ -2,14 +2,11 @@
   Warnings:
 
   - The primary key for the `Booking` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - The primary key for the `BookingItem` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to drop the column `roomId` on the `BookingItem` table. All the data in the column will be lost.
+  - You are about to drop the `BookingItem` table. If the table is not empty, all the data it contains will be lost.
   - You are about to drop the `Room` table. If the table is not empty, all the data it contains will be lost.
   - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
   - A unique constraint covering the columns `[referenceNo]` on the table `Booking` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `referenceNo` to the `Booking` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `unitId` to the `BookingItem` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `unitType` to the `BookingItem` table without a default value. This is not possible if the table is not empty.
 
 */
 -- DropForeignKey
@@ -24,29 +21,16 @@ ALTER TABLE `BookingItem` DROP FOREIGN KEY `BookingItem_roomId_fkey`;
 -- DropIndex
 DROP INDEX `Booking_userId_fkey` ON `Booking`;
 
--- DropIndex
-DROP INDEX `BookingItem_bookingId_fkey` ON `BookingItem`;
-
--- DropIndex
-DROP INDEX `BookingItem_roomId_fkey` ON `BookingItem`;
-
 -- AlterTable
 ALTER TABLE `Booking` DROP PRIMARY KEY,
-    ADD COLUMN `referenceNo` INTEGER NOT NULL AUTO_INCREMENT,
+    ADD COLUMN `referenceNo` INTEGER NOT NULL,
     MODIFY `id` VARCHAR(191) NOT NULL,
     MODIFY `status` ENUM('DRAFT', 'PENDING', 'CANCELLED', 'BOOKED', 'COMPLETED') NOT NULL DEFAULT 'BOOKED',
     MODIFY `userId` VARCHAR(191) NOT NULL,
     ADD PRIMARY KEY (`id`);
 
--- AlterTable
-ALTER TABLE `BookingItem` DROP PRIMARY KEY,
-    DROP COLUMN `roomId`,
-    ADD COLUMN `note` VARCHAR(191) NULL,
-    ADD COLUMN `unitId` VARCHAR(191) NOT NULL,
-    ADD COLUMN `unitType` ENUM('ROOM', 'BED') NOT NULL,
-    MODIFY `id` VARCHAR(191) NOT NULL,
-    MODIFY `bookingId` VARCHAR(191) NOT NULL,
-    ADD PRIMARY KEY (`id`);
+-- DropTable
+DROP TABLE `BookingItem`;
 
 -- DropTable
 DROP TABLE `Room`;
@@ -54,8 +38,22 @@ DROP TABLE `Room`;
 -- DropTable
 DROP TABLE `User`;
 
+-- CreateTable
+CREATE TABLE `BookingDetail` (
+    `id` VARCHAR(191) NOT NULL,
+    `count` INTEGER NOT NULL DEFAULT 1,
+    `note` VARCHAR(191) NULL,
+    `bookingId` VARCHAR(191) NOT NULL,
+    `itemId` VARCHAR(191) NOT NULL,
+    `itemType` ENUM('ROOM', 'BED') NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- CreateIndex
 CREATE UNIQUE INDEX `Booking_referenceNo_key` ON `Booking`(`referenceNo`);
 
 -- AddForeignKey
-ALTER TABLE `BookingItem` ADD CONSTRAINT `BookingItem_bookingId_fkey` FOREIGN KEY (`bookingId`) REFERENCES `Booking`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `BookingDetail` ADD CONSTRAINT `BookingDetail_bookingId_fkey` FOREIGN KEY (`bookingId`) REFERENCES `Booking`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
