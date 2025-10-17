40f12ec4d0fc0901b554c28835827638cb1e624e
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-10-17 15:53:26 +0700

    feat(image): first prototype of Image Service gRPC

--- API/image-service/src/index.ts
+++ API/image-service/src/index.ts
@@ -2,16 +2,37 @@ import express from "express";
 import ImageRouter from "./routes/ImageRouter";
 import ErrorHandler from "./middlewares/ErrorHandler";
 import { setupSwagger } from "./configs/swagger";
-const app = express();
-app.use(express.json());
+
+const restApp = express();
+restApp.use(express.json());
 
 // Base route: /image
 
-setupSwagger(app);
+setupSwagger(restApp);
 
-app.use(new ImageRouter().router);
+restApp.use(new ImageRouter().router);
 
-app.use(ErrorHandler.handle);
+restApp.use(ErrorHandler.handle);
 
 const PORT = process.env["PORT"] || 3007;
-app.listen(PORT, () => console.log(`Image Service running on port ${PORT}`));
+restApp.listen(PORT, () => console.log(`Image Service running on port ${PORT}`));
+
+// ============================================================================================== //
+
+import { Server, ServerCredentials } from "@grpc/grpc-js";
+import { ImageServiceService } from "../generated/grpc/image-service";
+import { getHealthHandler } from "./handlers/getHealthHandler";
+const server = new Server();
+
+server.addService(ImageServiceService, {
+    getHealth: getHealthHandler,
+});
+
+server.bindAsync("0.0.0.0:50057", ServerCredentials.createInsecure(), (error, port) => {
+    if (error) {
+        console.error(`Server binding failed: ${error.message}`);
+        return;
+    }
+    console.log(`Server running at http://0.0.0.0:${port}`);
+    // server.start()
+});
