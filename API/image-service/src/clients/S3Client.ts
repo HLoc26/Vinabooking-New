b049ea90a3e5a96106b40a2df2aa23d1fa45b64d
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-10-19 22:15:42 +0700

    feat(image): update s3Client to public region

--- API/image-service/src/clients/S3Client.ts
+++ API/image-service/src/clients/S3Client.ts
@@ -3,11 +3,12 @@ import { S3Client } from "@aws-sdk/client-s3";
 class S3ClientSingleton {
     private static s3: S3Client;
     public static readonly bucketName = process.env["AWS_BUCKET_NAME"];
+    public static readonly region = process.env["AWS_REGION"];
     constructor() {}
     public static getInstance() {
         if (!S3ClientSingleton.s3) {
             S3ClientSingleton.s3 = new S3Client({
-                region: process.env["AWS_REGION"]!,
+                region: S3ClientSingleton.region,
                 credentials: {
                     accessKeyId: process.env["AWS_ACCESS_KEY_ID"]!,
                     secretAccessKey: process.env["AWS_SECRET_ACCESS_KEY"]!,
