0b832d60b9c899fe83467a8a318c7ea7c36c0200
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-10-19 14:43:31 +0700

    feat(image): update grpc to handle uploading

--- API/image-service/src/index.ts
+++ API/image-service/src/index.ts
@@ -20,11 +20,17 @@ restApp.listen(PORT, () => console.log(`Image Service running on port ${PORT}`))
 
 import { Server, ServerCredentials } from "@grpc/grpc-js";
 import { ImageServiceService } from "../generated/grpc/image-service/image-service";
-import { getHealthHandler } from "./handlers/getHealthHandler";
+import GrpcImageController from "./controllers/gRPCImageController";
+import { UploadService } from "./services/UploadService";
+import S3Service from "./services/S3Service";
+import ImageRepository from "./repositories/ImageRepository";
 const server = new Server();
 
+const grpcImageController = new GrpcImageController(new UploadService(new S3Service(), new ImageRepository()));
+
 server.addService(ImageServiceService, {
-    getHealth: getHealthHandler,
+    getHealth: grpcImageController.getHealth,
+    uploadImages: grpcImageController.uploadImages.bind(grpcImageController),
 });
 
 server.bindAsync("0.0.0.0:50057", ServerCredentials.createInsecure(), (error, port) => {
--- API/image-service/src/services/UploadService.ts
+++ API/image-service/src/services/UploadService.ts
@@ -5,6 +5,8 @@ import { CreateOptimized, CreateThumbnail, CreateWEBP, ImageProcessingPipeline }
 import S3Service from "./S3Service";
 import { EEntityType, EVariantType, Image } from "../../generated/prisma/index.js";
 
+import { EEntityType as GRPC_EEntityType } from "../../generated/grpc/image-service/image-service";
+
 export class UploadService {
     constructor(
         private readonly s3Service: S3Service,
@@ -28,6 +30,74 @@ export class UploadService {
         const uploadResult = await uploadFn(entityId, processed, original.mimetype);
         const saved = await saveFn(entityId, uploadResult, original);
         return Boolean(saved?.id && saved?.s3Key);
+    }
+
+    /**
+     * Gộp xử lý upload cho tất cả entity
+     * @param entityType loại entity (USER_PROFILE, ACCOMMODATION, ROOM, REVIEW)
+     * @param entityId id entity
+     * @param files danh sách file upload
+     */
+    async handleUploadByEntity(entityType: EEntityType | GRPC_EEntityType, entityId: string, files: FileType[]) {
+        if (!files?.length) throw new BadRequestError("Empty files");
+
+        switch (entityType) {
+            case EEntityType.USER_PROFILE:
+            case GRPC_EEntityType.USER_PROFILE:
+                // User chỉ upload 1 file
+                return this.processAndUpload(
+                    entityId,
+                    files[0],
+                    this.s3Service.uploadProfileImage.bind(this.s3Service),
+                    this.imageRepository.saveProfileImage.bind(this.imageRepository)
+                );
+
+            case EEntityType.ACCOMMODATION:
+            case GRPC_EEntityType.ACCOMMODATION:
+                await Promise.all(
+                    files.map((file) =>
+                        this.processAndUpload(
+                            entityId,
+                            file,
+                            this.s3Service.uploadAccommodationImages.bind(this.s3Service),
+                            this.imageRepository.saveEntityImage.bind(this.imageRepository, EEntityType.ACCOMMODATION)
+                        )
+                    )
+                );
+                return true;
+
+            case EEntityType.ROOM:
+            case GRPC_EEntityType.ROOM:
+                await Promise.all(
+                    files.map((file) =>
+                        this.processAndUpload(
+                            entityId,
+                            file,
+                            this.s3Service.uploadRoomImages.bind(this.s3Service),
+                            this.imageRepository.saveEntityImage.bind(this.imageRepository, EEntityType.ROOM)
+                        )
+                    )
+                );
+                return true;
+
+            case EEntityType.REVIEW:
+            case GRPC_EEntityType.REVIEW:
+                await Promise.all(
+                    files.map((file) =>
+                        this.processAndUpload(
+                            entityId,
+                            file,
+                            this.s3Service.uploadReviewImages.bind(this.s3Service),
+                            this.imageRepository.saveEntityImage.bind(this.imageRepository, EEntityType.REVIEW)
+                        )
+                    )
+                );
+                return true;
+
+            default:
+                console.log(entityType, GRPC_EEntityType.ACCOMMODATION);
+                throw new BadRequestError("Unsupported entity type");
+        }
     }
 
     async handleUserProfileUpload(userId: string, original: FileType) {
