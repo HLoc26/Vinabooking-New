e6cf99ea820488fe09e90f38bde6101322dceb63
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-10-19 23:15:11 +0700

    refactor(image): move entityTypeMapping method to MappingUtil class for reuse

---
+++ API/image-service/src/utils/MappingUtil.ts
@@ -0,0 +1,22 @@
+import { EEntityType } from "../../generated/prisma";
+
+import { EEntityType as GRPC_EEntityType } from "../../generated/grpc/image-service/image-service";
+
+class MappingUtil {
+    public static entityTypeMapping(type: EEntityType | GRPC_EEntityType): EEntityType {
+        switch (type) {
+            case GRPC_EEntityType.USER_PROFILE:
+                return EEntityType.USER_PROFILE;
+            case GRPC_EEntityType.ACCOMMODATION:
+                return EEntityType.ACCOMMODATION;
+            case GRPC_EEntityType.ROOM:
+                return EEntityType.ROOM;
+            case GRPC_EEntityType.REVIEW:
+                return EEntityType.REVIEW;
+            default:
+                return type as EEntityType;
+        }
+    }
+}
+
+export default MappingUtil;
