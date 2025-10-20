0deb85bc2db9dda3bf45597aa182957e152c792c
sang-ute <nqs28012004@gmail.com>
2025-10-20 22:28:34 +0700

    fix(routing): remove unwanted routing of /booking

--- API/booking-service/src/index.ts
+++ API/booking-service/src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 const bookingRouter = BookingRouterFactory.createBookingRouter();
-app.use("/booking", bookingRouter);
+app.use("/", bookingRouter);
 
 app.get("/health", (_req, res) => res.json({ service: "Booking Service", success: true }));
 
