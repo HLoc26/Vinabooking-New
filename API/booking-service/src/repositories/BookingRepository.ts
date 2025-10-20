35032dbfa3f6d1572a60510a8144f254df89b72c
sang-ute <nqs28012004@gmail.com>
2025-10-20 20:27:36 +0700

    refactor(booking): reorganize methods in BookingRepository for improved readability

--- API/booking-service/src/repositories/BookingRepository.ts
+++ API/booking-service/src/repositories/BookingRepository.ts
@@ -2,20 +2,39 @@ import PrismaSingleton from "../clients/PrismaSingleton";
 
 export default class BookingRepository {
     private prisma = PrismaSingleton.getInstance();
-    
-    public async findByUserId(userId: string) {
-        return await this.prisma.booking.findMany({
-            where: { userId },
-            include: { details: true },
-    });
-}
 
     public async findById(id: string) {
         return await this.prisma.booking.findUnique({
             where: { id },
-            include: {
-                details: true,
-            },
+            include: { details: true },
+        });
+    }
+
+    public async findByUserId(userId: string) {
+        return await this.prisma.booking.findMany({
+            where: { userId },
+            include: { details: true },
+        });
+    }
+
+    public async findByRoomId(roomId: string) {
+        return await this.prisma.booking.findMany({
+            where: { details: { some: { itemId: roomId, itemType: "ROOM" } } },
+            include: { details: true },
+        });
+    }
+
+    // public async findByAccommodationId(accommodationId: string) {
+    //     return await this.prisma.booking.findMany({
+    //         where: { details: { some: { itemId: accommodationId, itemType: "ACCOMMODATION" } } },
+    //         include: { details: true },
+    //     });
+    // }
+
+    public async createBooking(data: any) {
+        return await this.prisma.booking.create({
+            data,
+            include: { details: true },
         });
     }
 }
