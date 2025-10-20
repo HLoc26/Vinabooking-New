0f623d042af30a7cac2fd3ea0f4bb1ccfdec73bd
sang-ute <nqs28012004@gmail.com>
2025-10-20 18:24:28 +0700

    feat(repository): add findByUserId method to BookingRepository for retrieving bookings by user ID

--- API/booking-service/src/repositories/BookingRepository.ts
+++ API/booking-service/src/repositories/BookingRepository.ts
@@ -2,6 +2,12 @@ import PrismaSingleton from "../clients/PrismaSingleton";
 
 export default class BookingRepository {
     private prisma = PrismaSingleton.getInstance();
+    public async findByUserId(userId: string) {
+        return await this.prisma.booking.findMany({
+            where: { userId },
+            include: { details: true },
+    });
+}
 
     public async findById(id: string) {
         return await this.prisma.booking.findUnique({
