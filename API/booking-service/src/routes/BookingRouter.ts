f7b128dca6f260424a8b4d48482d3e21b964a9a2
sang-ute <nqs28012004@gmail.com>
2025-10-20 18:11:31 +0700

    feat(booking): implement BookingRouter with health check and booking retrieval routes

---
+++ API/booking-service/src/routes/BookingRouter.ts
@@ -0,0 +1,37 @@
+import { Router, type Request, type Response } from "express";
+import ResponseHelper from "../utils/ResponseHelper";
+import BookingController from "../controllers/BookingController";
+import BookingRepository from "../repositories/BookingRepository";
+import BookingService from "../services/BookingService";
+
+class BookingRouterFactory {
+    static createBookingRouter() {
+        const bookingRepository = new BookingRepository();
+        const bookingService = new BookingService(bookingRepository);
+        const bookingController = new BookingController(bookingService);
+
+        const bookingRouter = new BookingRouter(bookingController, Router());
+        return bookingRouter.router;
+    }
+}
+
+class BookingRouter {
+    constructor(
+        private readonly bookingController: BookingController,
+        public readonly router: Router
+    ) {
+        this.registerRoutes();
+    }
+
+    private registerRoutes() {
+        this.router.get("/health", (_req: Request, res: Response) => {
+            return ResponseHelper.success(res, { service: "Booking Service", success: true });
+        });
+
+        this.router.get("/:id", (req: Request, res: Response) => {
+            return this.bookingController.getBookingById(req, res);
+        });
+    }
+}
+
+export default BookingRouterFactory;
