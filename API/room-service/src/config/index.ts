03acd9b8b436421b7ba637a449586d7874710c93
zhwy512 <huyngh05@gmail.com>
2025-10-20 21:12:49 +0700

    feat(room): add configuration file with environment variables

---
+++ API/room-service/src/config/index.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+    port: process.env.PORT || 3005,
+    roomEndpoint: process.env.ACCOMMODATION_ENDPOINT || "http://localhost:3001",
+    imageEndpoint: process.env.IMAGE_ENDPOINT || "http://localhost:3007",
+};
