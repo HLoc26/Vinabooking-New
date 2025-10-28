a0bd3b9e6dab5817dc24060d958cd1e50e640dea
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-10-28 12:10:41 +0700

    feat(api-gateway): implement cors

--- API/api-gateway/src/index.js
+++ API/api-gateway/src/index.js
@@ -1,10 +1,20 @@
 import express from "express";
 import proxy from "express-http-proxy";
+import cors from "cors";
 
 import "dotenv/config";
 
 const app = express();
 app.use(express.json());
+
+app.use(
+    cors({
+        origin: "http://localhost:5173", // hoặc '*' nếu đang dev
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    })
+);
 
 // Middleware check auth cho tất cả request
 app.use((req, res, next) => {
