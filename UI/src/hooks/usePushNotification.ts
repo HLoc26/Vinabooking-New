e7c6b95b9ca581d2943e37ba72e11a2910c4089a
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-10-28 09:03:33 +0700

    feat(notification): update delete logic and push noti's animation

--- UI/src/hooks/usePushNotification.ts
+++ UI/src/hooks/usePushNotification.ts
@@ -7,15 +7,25 @@ export interface Notification {
     severity: AlertColor;
 }
 
+const MAX_NOTIFICATIONS = 5;
+
 export function usePushNotification() {
 	const [notifications, setNotifications] = useState<Notification[]>([]);
 
 	const pushNotification = useCallback((message: string, severity: AlertColor = "info") => {
 		const id = Date.now().toString();
 		const newNotification = { id, message, severity };
-		setNotifications((prev) => [...prev, newNotification]);
 
-		// Hides after 3s
+		setNotifications((prev) => {
+			const updated = [...prev, newNotification];
+			// Nếu vượt quá 5 thông báo => loại bỏ cái cũ nhất
+			if (updated.length > MAX_NOTIFICATIONS) {
+				updated.shift();
+			}
+			return updated;
+		});
+
+		// Tự ẩn sau 3 giây
 		setTimeout(() => {
 			setNotifications((prev) => prev.filter((n) => n.id !== id));
 		}, 3000);
