851dbdcf4fab5e582a2247213a7dcc4a98853b1a
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-10-28 11:49:00 +0700

    fix(user): use user role when sign up

---
+++ API/user-service/src/utils/UserRoleMapper.ts
@@ -0,0 +1,5 @@
+import { EUserRole } from "../types/User";
+
+export const userRoleMapper = (userType: "TRAVELLER" | "ACCOMMODATION_OWNER") => {
+    return userType === "TRAVELLER" ? EUserRole.TRAVELLER : EUserRole.ACCOMMODATION_OWNER;
+};
