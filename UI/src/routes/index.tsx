535dd7918b7932ec0a2bb16c8d5d01c49cf7e81b
zhwy512 <huyngh05@gmail.com>
2025-10-28 15:54:19 +0700

    feat(ui/accommodation): add route for detail page

--- UI/src/routes/index.tsx
+++ UI/src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from '../features/common/pages/HomePage';
+import AccommodationDetailPage from '../features/accommodation/pages/DetailPage';
 
 export const AppRouter = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/accommodation/:accommodationId" element={<AccommodationDetailPage />} />
         </Routes>
     </BrowserRouter>
 );
