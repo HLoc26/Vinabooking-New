4a6855233045586ec621446c5f49a82c47e70b46
sang-ute <nqs28012004@gmail.com>
2025-10-28 17:56:18 +0700

    feat(booking): implement booking preview page with user and accommodation details

--- UI/src/routes/index.tsx
+++ UI/src/routes/index.tsx
@@ -3,13 +3,14 @@ import { HomePage } from "../features/common/pages/HomePage";
 
 import { OAuthRouter } from "./oauth";
 import { AuthRouter } from "./auth";
-
+import { BookingPreviewPage } from "../features/booking/pages/BookingPreviewPage";
 export const AppRouter = () => (
 	<BrowserRouter>
 		<Routes>
 			<Route path="/" element={<HomePage />} />
 			<Route path="/auth/*" element={<AuthRouter />} />
 			<Route path="/oauth/*" element={<OAuthRouter />} />
+			<Route path="/booking" element={<BookingPreviewPage />} />
 		</Routes>
 	</BrowserRouter>
 );
