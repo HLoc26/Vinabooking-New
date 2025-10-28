c4631dd8f736ccd6cbd725d7b837ce83b4ad5f9a
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-10-28 16:14:08 +0700

    feat(auth): implement useAuth hook for user authentication

--- UI/src/features/user/types/Response.ts
+++ UI/src/features/user/types/Response.ts
@@ -1,30 +1,42 @@
 export interface AuthResponse {
-    token: string;
-    user: {
-        id: string;
-        name: string;
-        email: string;
-        userType: "TRAVELLER" | "ACCOMMODATION_OWNER";
-    };
+	token: string;
+	user: {
+		id: string;
+		name: string;
+		email: string;
+		userType: "TRAVELLER" | "ACCOMMODATION_OWNER";
+	};
 }
 
 export interface SignUpResponse {
-    UserSub: string | undefined;
-    CodeDeliveryDestination?: string | undefined;
-    CodeDeliveryMedium?: "EMAIL" | "SMS" | undefined;
+	UserSub: string | undefined;
+	CodeDeliveryDestination?: string | undefined;
+	CodeDeliveryMedium?: "EMAIL" | "SMS" | undefined;
 }
 
 export interface GetOTPResponse {
-    CodeDeliveryDestination?: string;
-    CodeDeliveryMedium?: string;
+	CodeDeliveryDestination?: string;
+	CodeDeliveryMedium?: string;
 }
 
 export interface ConfirmUserResponse {
-    success: boolean;
+	success: boolean;
+}
+
+export interface LogInResponse {
+	accessToken: string;
+	idToken: string;
+	expiresIn: number;
+	tokenType: string | "Bearer";
+	user: {
+		id: string;
+		name: string;
+		email: string;
+	};
 }
 
 export interface ApiResponse<T> {
-    success: boolean;
-    data?: T;
-    error?: string | null;
+	success: boolean;
+	data?: T;
+	error?: string | null;
 }
