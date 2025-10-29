55ad20870f07b25439bc539d2d680e49659615c9
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-10-29 21:40:50 +0700

    feat(user): implement saveUserDirect method in UserController and UserService

--- API/user-service/src/routes/UserRouter.ts
+++ API/user-service/src/routes/UserRouter.ts
@@ -1,7 +1,7 @@
 import express, { type Router, type Request, type Response } from "express";
 import ResponseHelper from "../utils/ResponseHelper";
 import UserController from "../controllers/UserController";
-import type { FindUserByIdRequest } from "../types/Request";
+import type { FindUserByIdRequest, SaveUserDirectRequest, SaveUserRequest } from "../types/Request";
 import { getRedisClient } from "../clients/RedisSingleton"; // thay đổi từ default export sang function
 
 // Base route: /users
@@ -24,6 +24,10 @@ class UserRouter {
                 user_service: "Healthy",
                 redis: ping,
             });
+        });
+
+        this.router.post("/", (req: SaveUserDirectRequest, res: Response) => {
+            return this.userController.saveUserDirect(req, res);
         });
 
         // Get user by Id
