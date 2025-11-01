f789e4c5ee53afcf6caff2b39a0fa7cbad0e56d2
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-02 02:15:32 +0700

    feat(routes): refactor OAuth and Auth routes into separate component

--- UI/src/routes/index.tsx
+++ UI/src/routes/index.tsx
@@ -1,20 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "../features/common/pages/HomePage";
-import LoginPage from "../features/user/pages/LoginPage";
-import RegisterPage from "../features/user/pages/RegisterPage";
-import ConfirmOTPPage from "../features/user/pages/ConfirmPage";
-import { OAuthSuccessPage } from "../features/user/pages/OAuthSuccessPage";
-import { OAuthErrorPage } from "../features/user/pages/OAuthErrorPage";
+
+import { OAuthRouter } from "./oauth";
+import { AuthRouter } from "./auth";
 
 export const AppRouter = () => (
 	<BrowserRouter>
 		<Routes>
 			<Route path="/" element={<HomePage />} />
-			<Route path="/login" element={<LoginPage />} />
-			<Route path="/register" element={<RegisterPage />} />
-			<Route path="/otp" element={<ConfirmOTPPage />} />
-			<Route path="/oauth/success" element={<OAuthSuccessPage />} />
-			<Route path="/oauth/error" element={<OAuthErrorPage />} />
+			<Route path="/auth/*" element={<AuthRouter />} />
+			<Route path="/oauth/*" element={<OAuthRouter />} />
 		</Routes>
 	</BrowserRouter>
 );
