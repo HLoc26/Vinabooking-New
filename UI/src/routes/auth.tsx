1996ef1070107b03a381529e07a89fd45219c9f0
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-03 10:10:19 +0700

    feat(auth): add route for forgot password pages

--- UI/src/routes/auth.tsx
+++ UI/src/routes/auth.tsx
@@ -2,11 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "../features/user/pages/LoginPage";
 import RegisterPage from "../features/user/pages/RegisterPage";
 import ConfirmOTPPage from "../features/user/pages/ConfirmPage";
+import ForgotPasswordRequestPage from "../features/user/pages/ForgotPasswordRequestPage";
+import ForgotPasswordVerifyPage from "../features/user/pages/ForgotPasswordVerifyPage";
 
 export const AuthRouter = () => (
 	<Routes>
 		<Route path="/login" element={<LoginPage />} />
 		<Route path="/register" element={<RegisterPage />} />
 		<Route path="/otp" element={<ConfirmOTPPage />} />
+		<Route path="/forgot-password" element={<ForgotPasswordRequestPage />} />
+		<Route path="/forgot-password/verify" element={<ForgotPasswordVerifyPage />} />
 	</Routes>
 );
