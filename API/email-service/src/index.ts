4f7c37876b9c57bb3d3c1d7a5e9ae13660c052f1
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-06 17:11:53 +0700

    feat(email): add swagger.yaml and serve it

--- API/email-service/src/index.ts
+++ API/email-service/src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import ErrorHandler from "./middlewares/ErrorHandler";
 import EmailRouterFactory from "./routes/EmailRouterFactory";
+import { setupSwagger } from "./config/swagger";
 const app = express();
 app.use(express.json());
 
+setupSwagger(app);
+
 // Base route: /users
-app.use("/", EmailRouterFactory.createEmailRouter());
+app.use(EmailRouterFactory.createEmailRouter());
 
 app.use(ErrorHandler.handle);
 
 const PORT = process.env["PORT"] || 3008;
-app.listen(PORT, () => console.log(`User Service running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Email Service running on port ${PORT}`));
