f8934ff165e02abe461e550b0a1d349b52b7fb7d
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-06 16:36:11 +0700

    feat(email): implement send route for email service

--- API/email-service/src/routes/EmailRouterFactory.ts
+++ API/email-service/src/routes/EmailRouterFactory.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import EmailRouter from "./EmailRouter";
 import EmailController from "../controllers/EmailController";
+import SmtpClient from "../clients/SmtpClient";
 
 class EmailRouterFactory {
     public static createEmailRouter() {
         const router = Router();
-        const emailController = new EmailController();
+        const smtpClient = new SmtpClient();
+        const emailController = new EmailController(smtpClient);
         return new EmailRouter(emailController, router).router;
     }
 }
