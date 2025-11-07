2fc3380068f67042f896728cf70a7dc9f68be521
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-07 10:32:03 +0700

    feat(email): splitting RestServer into seperated file, and add gRPC server

--- API/email-service/src/index.ts
+++ API/email-service/src/index.ts
@@ -1,16 +1,8 @@
-import express from "express";
-import ErrorHandler from "./middlewares/ErrorHandler";
-import EmailRouterFactory from "./routes/EmailRouterFactory";
-import { setupSwagger } from "./config/swagger";
-const app = express();
-app.use(express.json());
+import GrpcServerFactory from "./GrpcServer";
+import RestServer from "./restServer";
 
-setupSwagger(app);
+const restServer = new RestServer();
+restServer.start();
 
-// Base route: /users
-app.use(EmailRouterFactory.createEmailRouter());
-
-app.use(ErrorHandler.handle);
-
-const PORT = process.env["PORT"] || 3008;
-app.listen(PORT, () => console.log(`Email Service running on port ${PORT}`));
+const grpcServer = GrpcServerFactory.createGrpcServer();
+grpcServer.start();
