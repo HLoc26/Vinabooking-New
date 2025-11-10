1997c708ceba11874fb9b4960d7d1824e1dce104
sang-ute <nqs28012004@gmail.com>
2025-11-10 23:16:14 +0700

    feat(booking): update booking types and API for improved data handling

--- UI/src/features/booking/services/bookingApi.ts
+++ UI/src/features/booking/services/bookingApi.ts
@@ -1,12 +1,13 @@
 // src/features/booking/services/bookingApi.ts
 import axios from "axios";
-import type { BookingDto } from "../types/BookingDto";
+import type { BookingDto, BookingImageDto } from "./types/BookingDto";
 import Cookies from "js-cookie";
 
 const ACCESS_TOKEN_KEY = import.meta.env.VITE_ACCESS_TOKEN_KEY;
 const token = Cookies.get(ACCESS_TOKEN_KEY);
 const API_URL = "http://localhost:3000";
 const BOOKING_ENDPOINT = `${API_URL}/bookings`; // old backend uses /booking route
+const ACCOMMODATION_ENDPOINT = `${API_URL}/accommodations`;
 
 export const bookingApi = {
   /**
@@ -19,10 +20,10 @@ export const bookingApi = {
       endDate: data.endDate,
       guestCount: data.guestCount,
       phone: data.user.phone,
-      status: "PENDING",
-      userId: "mock-user-123", // for now
+      status: null || "PENDING",
+      userId: data.user.id || "mock-user-123", // for now
       details: {
-        create: data.rooms.map((room) => ({
+        create: data.room.map((room) => ({
           itemId: room.id,
           itemType: room.type,
           count: 1,
@@ -40,17 +41,19 @@ export const bookingApi = {
 
     return res.data;
   },
-  //   async getAccomImage(){
-  //     const res = await axios.get(ACCOMMODATION_ENDPOINT, {
-  //       headers: {
-  //         Authorization: `Bearer ${token ?? "mock-jwt-token"}`,
-  //       },
-  //       params: {
-  //         byEntity: "room",
-  //         entityId: "r2-uuid",
-  //       },
-  //   }),
-  // };
+  async getAccomImage(data: BookingImageDto) {
+    const res = await axios.get(ACCOMMODATION_ENDPOINT, {
+      headers: {
+        Authorization: `Bearer ${token ?? "mock-jwt-token"}`,
+      },
+      params: {
+        byEntity: data.entity,
+        entityId: data.id,
+      },
+    });
+    return res.data;
+  },
+
   async getBooking(id: string) {
     const res = await axios.get(`${BOOKING_ENDPOINT}/${id}`);
     return res.data;
