bb016277fde1a522ae1e1e85dfe25bcbd3477175
sang-ute <nqs28012004@gmail.com>
2025-11-10 23:38:35 +0700

    feat(booking): update BookingDto imports and refactor booking data handling in components

--- UI/src/features/booking/hooks/useConfirmBooking.ts
+++ UI/src/features/booking/hooks/useConfirmBooking.ts
@@ -1,20 +1,20 @@
 // src/features/booking/hooks/useConfirmBooking.ts
 import { useState } from "react";
 import { bookingApi } from "../services/bookingApi";
-import type { BookingDto } from "../types/BookingDto";
+import type { BookingDto } from "../services/types/BookingDto";
 
 export function useConfirmBooking() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-const confirmBooking = async (booking: BookingDto) => {
-  try {
-    const response = await bookingApi.createBooking(booking);
-    console.log("Booking created:", response);
-  } catch (err) {
-    console.error("Error confirming booking:", err);
-  }
-};
+  const confirmBooking = async (booking: BookingDto) => {
+    try {
+      const response = await bookingApi.createBooking(booking);
+      console.log("Booking created:", response);
+    } catch (err) {
+      console.error("Error confirming booking:", err);
+    }
+  };
 
 
   return { confirmBooking, loading, error };
