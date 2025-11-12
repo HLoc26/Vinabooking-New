858993c115dd79d26b038a1e77f85c041fc8ebf8
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-12 11:27:53 +0700

    refactor(auth): update indent

--- API/auth-service/src/routes/AuthRouter.ts
+++ API/auth-service/src/routes/AuthRouter.ts
@@ -5,71 +5,71 @@ import { GetOTPRequest } from "../types/Request";
 
 // Base route: /auth
 class AuthRouter {
-    constructor(
-        public router: Router,
-        private authController: AuthController
-    ) {
-        this.registerRoutes();
-    }
+	constructor(
+		public router: Router,
+		private authController: AuthController
+	) {
+		this.registerRoutes();
+	}
 
-    private registerRoutes() {
-        this.router.get("/health", (_req: Request, res: Response) => {
-            return ResponseHelper.success(res, { service: "Auth Service", success: true });
-        });
+	private registerRoutes() {
+		this.router.get("/health", (_req: Request, res: Response) => {
+			return ResponseHelper.success(res, { service: "Auth Service", success: true });
+		});
 
-        this.router.post(
-            "/sign-up",
-            // First sign up using Cognito
-            (req: Request, res: Response, next: NextFunction) => {
-                return this.authController.signUp(req, res, next);
-            },
-            // Then cache the user email and sub to cache
-            (req: Request, res: Response) => {
-                return this.authController.cacheUser(req, res);
-            }
-        );
+		this.router.post(
+			"/sign-up",
+			// First sign up using Cognito
+			(req: Request, res: Response, next: NextFunction) => {
+				return this.authController.signUp(req, res, next);
+			},
+			// Then cache the user email and sub to cache
+			(req: Request, res: Response) => {
+				return this.authController.cacheUser(req, res);
+			}
+		);
 
-        this.router.get("/otp", (req: Request, res: Response) => {
-            const request = req as unknown as GetOTPRequest;
-            return this.authController.getNewOtp(request, res);
-        });
+		this.router.get("/otp", (req: Request, res: Response) => {
+			const request = req as unknown as GetOTPRequest;
+			return this.authController.getNewOtp(request, res);
+		});
 
-        this.router.post(
-            "/sign-up/confirm",
-            // First confirm the OTP code
-            (req: Request, res: Response, next: NextFunction) => {
-                return this.authController.confirmUser(req, res, next);
-            },
-            // Then if confirm success, save user to db
-            (req: Request, res: Response) => {
-                return this.authController.saveUser(req, res);
-            }
-        );
+		this.router.post(
+			"/sign-up/confirm",
+			// First confirm the OTP code
+			(req: Request, res: Response, next: NextFunction) => {
+				return this.authController.confirmUser(req, res, next);
+			},
+			// Then if confirm success, save user to db
+			(req: Request, res: Response) => {
+				return this.authController.saveUser(req, res);
+			}
+		);
 
-        this.router.post("/log-in", (req: Request, res: Response) => {
-            return this.authController.logIn(req, res);
-        });
+		this.router.post("/log-in", (req: Request, res: Response) => {
+			return this.authController.logIn(req, res);
+		});
 
-        this.router.post("/refresh", (req: Request, res: Response) => {
-            return this.authController.refreshToken(req, res);
-        });
+		this.router.post("/refresh", (req: Request, res: Response) => {
+			return this.authController.refreshToken(req, res);
+		});
 
-        this.router.post("/verify", (req: Request, res: Response) => {
-            return this.authController.verifyToken(req, res);
-        });
+		this.router.post("/verify", (req: Request, res: Response) => {
+			return this.authController.verifyToken(req, res);
+		});
 
-        this.router.post("/sign-out", this.authController.signOut.bind(this.authController));
+		this.router.post("/sign-out", this.authController.signOut.bind(this.authController));
 
-        this.router.get("/google/callback", this.authController.googleCallback.bind(this.authController));
+		this.router.get("/google/callback", this.authController.googleCallback.bind(this.authController));
 
-        this.router.post("/forgot-password", (req: Request, res: Response) => {
-            return this.authController.forgotPassword(req, res);
-        });
+		this.router.post("/forgot-password", (req: Request, res: Response) => {
+			return this.authController.forgotPassword(req, res);
+		});
 
-        this.router.post("/forgot-password/confirm", (req: Request, res: Response) => {
-            return this.authController.confirmForgotPassword(req, res);
-        });
-    }
+		this.router.post("/forgot-password/confirm", (req: Request, res: Response) => {
+			return this.authController.confirmForgotPassword(req, res);
+		});
+	}
 }
 
 export default AuthRouter;
