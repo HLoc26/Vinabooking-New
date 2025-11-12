e74d22d580bde18e885f82154683bb6f07b76d47
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-12 11:37:19 +0700

    refactor(user): update indent

--- API/user-service/src/errors/RedisClientError.ts
+++ API/user-service/src/errors/RedisClientError.ts
@@ -1,9 +1,9 @@
 import AppError from "./AppError";
 
 class RedisClientError extends AppError {
-    constructor(message = "Unknown problem with Redis client") {
-        super(message, 500);
-    }
+	constructor(message = "Unknown problem with Redis client") {
+		super(message, 500);
+	}
 }
 
 export default RedisClientError;
--- API/user-service/src/types/Favourite.ts
+++ API/user-service/src/types/Favourite.ts
@@ -1,15 +1,15 @@
 import { type FavouriteList as FavouriteListSchema, type FavouriteItem as FavouriteItemSchema } from "../../generated/prisma/index.js";
 
 export interface IFavouriteList {
-    id: string;
-    items: Array<IFavouriteItem>;
-    createdAt?: Date;
-    updatedAt?: Date;
+	id: string;
+	items: Array<IFavouriteItem>;
+	createdAt?: Date;
+	updatedAt?: Date;
 }
 
 export interface IFavouriteItem {
-    id: string;
-    accommodationId: string;
+	id: string;
+	accommodationId: string;
 }
 
 export type FavouriteListWithItems = { items: FavouriteItemSchema[] } & FavouriteListSchema;
