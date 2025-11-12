c2aaf54c18d296f2d90f156fe081fc7654b6400d
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-12 11:32:16 +0700

    refactor(image): update indent

--- API/image-service/src/routes/ImageRouter.ts
+++ API/image-service/src/routes/ImageRouter.ts
@@ -9,42 +9,42 @@ import { UploadService } from "../services/UploadService";
 import UploadClient from "../clients/UploadClient";
 
 class ImageRouterFactory {
-    static createImageRouter() {
-        const s3Service = new S3Service();
-        const imageRepository = new ImageRepository();
-        const uploadService = new UploadService(s3Service, imageRepository);
-        const imageController = new ImageController(uploadService, imageRepository, s3Service);
-        const uploadClient = UploadClient.getInstance();
-        const imageRouter = new ImageRouter(imageController, Router(), uploadClient);
-        return imageRouter.router;
-    }
+	static createImageRouter() {
+		const s3Service = new S3Service();
+		const imageRepository = new ImageRepository();
+		const uploadService = new UploadService(s3Service, imageRepository);
+		const imageController = new ImageController(uploadService, imageRepository, s3Service);
+		const uploadClient = UploadClient.getInstance();
+		const imageRouter = new ImageRouter(imageController, Router(), uploadClient);
+		return imageRouter.router;
+	}
 }
 
 // Base route: /images
 class ImageRouter {
-    constructor(
-        private readonly imageController: ImageController,
-        public readonly router: Router,
-        private readonly uploadClient: multer.Multer
-    ) {
-        this.registerRoutes();
-    }
+	constructor(
+		private readonly imageController: ImageController,
+		public readonly router: Router,
+		private readonly uploadClient: multer.Multer
+	) {
+		this.registerRoutes();
+	}
 
-    private registerRoutes() {
-        this.router.get("/health", (_req: Request, res: Response) => {
-            return ResponseHelper.success(res, { service: "Image Service", success: true });
-        });
+	private registerRoutes() {
+		this.router.get("/health", (_req: Request, res: Response) => {
+			return ResponseHelper.success(res, { service: "Image Service", success: true });
+		});
 
-        this.router.post("/:type/:id", this.uploadClient.array("files", 10), (req: Request, res: Response) => {
-            const uploadRequest = req as unknown as UploadRequest;
-            return this.imageController.upload(uploadRequest, res);
-        });
+		this.router.post("/:type/:id", this.uploadClient.array("files", 10), (req: Request, res: Response) => {
+			const uploadRequest = req as unknown as UploadRequest;
+			return this.imageController.upload(uploadRequest, res);
+		});
 
-        this.router.get("/:type/:id", (req: Request, res: Response) => {
-            const request = req as unknown as GetImagesRequest;
-            return this.imageController.getImages(request, res);
-        });
-    }
+		this.router.get("/:type/:id", (req: Request, res: Response) => {
+			const request = req as unknown as GetImagesRequest;
+			return this.imageController.getImages(request, res);
+		});
+	}
 }
 
 export default ImageRouterFactory;
