42afca6de93c8bb4ff91c02c1fed5deba463272f
sang-ute <nqs28012004@gmail.com>
2025-11-16 23:57:29 +0700

    feat(booking): update image fetching logic to support room and accommodation images

--- UI/src/features/booking/hooks/useFetchImages.ts
+++ UI/src/features/booking/hooks/useFetchImages.ts
@@ -1,5 +1,5 @@
 // src/features/booking/hooks/useFetchImages.ts
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { bookingApi } from "../services/bookingApi";
 import type { BookingImageDto } from "../services/types/BookingDto";
 
@@ -7,14 +7,20 @@ export function useFetchImages() {
 	const [loading] = useState(false);
 	const [error] = useState<string | null>(null);
 
-	const getAccomImage = async (booking: BookingImageDto) => {
+	const getImages = useCallback(async (data: BookingImageDto) => {
 		try {
-			const response = await bookingApi.getAccomImage(booking);
-			return response;
+			if (data.entity === "room") {
+				return await bookingApi.getRoomImage(data);
+			}
+			if (data.entity === "accommodation") {
+				return await bookingApi.getAccomImage(data);
+			}
+			return [];
 		} catch (err) {
 			console.error("Error fetching images:", err);
 			throw err;
 		}
-	};
-	return { getAccomImage, loading, error };
+	}, []); // Empty dependency array means this function never changes
+
+	return { getImages, loading, error };
 }
