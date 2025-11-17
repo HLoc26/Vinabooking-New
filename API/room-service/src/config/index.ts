d5210a0c4ece5de9cd047bcbb394d837b3e936d2
zhwy512 <huyngh05@gmail.com>
2025-11-17 20:16:43 +0700

    chore(accommodation): update environment variable defaults for endpoints

--- API/room-service/src/config/index.ts
+++ API/room-service/src/config/index.ts
@@ -1,3 +1,4 @@
+import { bookingClient } from "@src/clients/booking.client";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,4 +7,5 @@ export default {
     port: process.env.PORT || 3005,
     roomEndpoint: process.env.ACCOMMODATION_ENDPOINT || "http://localhost:3001",
     imageEndpoint: process.env.IMAGE_ENDPOINT || "http://localhost:3007",
+    bookingEndpoint: process.env.BOOKING_ENDPOINT || "http://localhost:3003",
 };
