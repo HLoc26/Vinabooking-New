62bd1bbd3ba42974cd8508bdab097ac8fa710d3c
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-18 14:40:45 +0700

    feat(ui): update apis to use common apiClient with refresh interceptor

--- UI/src/features/user/services/authApi.ts
+++ UI/src/features/user/services/authApi.ts
@@ -1,10 +1,5 @@
-import axios from "axios";
+import AxiosInstance from "../../../services/apiClient";
 import { type GetOTPResponse, type ApiResponse, type SignUpResponse, type ConfirmUserResponse, type LogInResponse, type ForgotPasswordSendOtpResponse } from "../types/Response";
-
-const api = axios.create({
-	baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
-	headers: { "Content-Type": "application/json" },
-});
 
 export const authApi = {
 	register: (payload: {
@@ -13,38 +8,34 @@ export const authApi = {
 		password: string;
 		phone: string;
 		userType: string;
-	}) => api.post<ApiResponse<SignUpResponse>>("/auth/sign-up", payload).then((r) => r.data),
+	}) => AxiosInstance.post<ApiResponse<SignUpResponse>>("/auth/sign-up", payload).then((r) => r.data),
 
 	resendOtp: (
 		email: string //
-	) => api.get<ApiResponse<GetOTPResponse>>(`/auth/otp?email=${email}`).then((r) => r.data),
+	) => AxiosInstance.get<ApiResponse<GetOTPResponse>>(`/auth/otp?email=${email}`).then((r) => r.data),
 
 	confirmOtp: (payload: { username: string; confirmCode: string }) =>
-		api //
-			.post<ApiResponse<ConfirmUserResponse>>("/auth/sign-up/confirm", payload)
+		AxiosInstance.post<ApiResponse<ConfirmUserResponse>>("/auth/sign-up/confirm", payload) //
 			.then((r) => r.data),
 
 	login: (payload: {
 		username: string; //
 		password: string;
-	}) => api.post<ApiResponse<LogInResponse>>("/auth/log-in", payload).then((r) => r.data),
+	}) => AxiosInstance.post<ApiResponse<LogInResponse>>("/auth/log-in", payload).then((r) => r.data),
 
 	signOut: (accessToken: string) =>
-		api
-			.post<ApiResponse<{ success: boolean }>>(
-				"/auth/sign-out",
-				{},
-				{
-					withCredentials: true,
-					headers: {
-						Authorization: `Bearer ${accessToken}`,
-					},
-				}
-			)
-			.then((r) => r.data),
+		AxiosInstance.post<ApiResponse<{ success: boolean }>>(
+			"/auth/sign-out",
+			{},
+			{
+				withCredentials: true,
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+				},
+			}
+		).then((r) => r.data),
 	forgotPassword: (email: string) =>
-		api
-			.post<ApiResponse<ForgotPasswordSendOtpResponse>>("/auth/forgot-password", { email }) //
+		AxiosInstance.post<ApiResponse<ForgotPasswordSendOtpResponse>>("/auth/forgot-password", { email }) //
 			.then((r) => r.data),
 
 	confirmForgotPassword: (payload: {
@@ -52,8 +43,7 @@ export const authApi = {
 		code: string;
 		newPassword: string;
 	}) =>
-		api
-			.post<ApiResponse<{ success: boolean }>>("/auth/forgot-password/confirm", payload) //
+		AxiosInstance.post<ApiResponse<{ success: boolean }>>("/auth/forgot-password/confirm", payload) //
 			.then((r) => r.data),
 };
 
