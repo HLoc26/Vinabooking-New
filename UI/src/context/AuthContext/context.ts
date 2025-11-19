affaf87b3cc48e00d557f517609a6f9d577d43ac
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-19 22:16:58 +0700

    fix(context): fix useAuth hook import

--- UI/src/context/AuthContext/context.ts
+++ UI/src/context/AuthContext/context.ts
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-import useAuth from "../../features/auth/hooks/useAuth";
+import useAuth from "../../hooks/useAuth";
 const AuthContext = createContext<ReturnType<typeof useAuth> | null>(null);
 
 export default AuthContext;
--- UI/src/context/AuthContext/provider.tsx
+++ UI/src/context/AuthContext/provider.tsx
@@ -1,4 +1,4 @@
-import useAuth from "../../features/auth/hooks/useAuth";
+import useAuth from "../../hooks/useAuth";
 import AuthContext from "./context";
 
 const AuthContextProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
