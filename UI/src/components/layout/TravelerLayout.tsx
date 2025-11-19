faf9598ab7c11eb6222e3f95e7792f4271608ca2
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-19 23:04:33 +0700

    fix(layout): remove container outside of children

--- UI/src/components/layout/TravelerLayout.tsx
+++ UI/src/components/layout/TravelerLayout.tsx
@@ -2,7 +2,6 @@ import React, { type ReactNode } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import NavigationBar from "../ui/NavigationBar";
 import Footer from "../ui/Footer";
-import { Container } from "@mui/material";
 
 type TravelerLayoutProps = {
 	children: ReactNode;
@@ -15,7 +14,7 @@ export const TravelerLayout: React.FC<TravelerLayoutProps> = ({ children }) => {
 			<NavigationBar />
 
 			{/* Main content */}
-			<Container>{children}</Container>
+			{children}
 
 			{/* Footer */}
 			<Footer />
