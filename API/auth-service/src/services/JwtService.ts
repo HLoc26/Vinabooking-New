29a83505c6d57d64b7d2869f7307d63417dfc6e3
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-19 13:42:56 +0700

    feat(auth): increase response timeout of verifier to 10s
    merge verifyAccessToken and verifyIdToken into verifyToken

--- API/auth-service/src/services/JwtService.ts
+++ API/auth-service/src/services/JwtService.ts
@@ -1,7 +1,33 @@
 import { CognitoJwtVerifier } from "aws-jwt-verify";
 import CognitoClient from "../clients/CognitoIdentityProviderClient";
+import { SimpleJwksCache } from "aws-jwt-verify/jwk";
+import { SimpleFetcher } from "aws-jwt-verify/https";
 
 class JwtService {
+	public static async verifyToken(token: string, type: "id" | "access") {
+		const verifier = CognitoJwtVerifier.create(
+			{
+				userPoolId: CognitoClient.userPoolId,
+				tokenUse: type,
+				clientId: CognitoClient.clientId,
+			},
+			{
+				jwksCache: new SimpleJwksCache({
+					fetcher: new SimpleFetcher({
+						defaultRequestOptions: {
+							responseTimeout: 10000,
+						},
+					}),
+				}),
+			}
+		);
+		const payload = await verifier.verify(token);
+		return payload;
+	}
+
+	/**
+	 * @deprecated
+	 */
 	public static async verifyAccessToken(token: string) {
 		const verifier = CognitoJwtVerifier.create({
 			userPoolId: CognitoClient.userPoolId,
@@ -12,6 +38,9 @@ class JwtService {
 		return payload;
 	}
 
+	/**
+	 * @deprecated
+	 */
 	public static async verifyIdToken(token: string) {
 		const verifier = CognitoJwtVerifier.create({
 			userPoolId: CognitoClient.userPoolId,
