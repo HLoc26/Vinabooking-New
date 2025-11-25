d8294fa699beb42734a647d2e2c75b440e694157
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-25 11:23:07 +0700

    fix(bookingApi): fix cancel booking route

--- UI/src/features/user/services/bookingApi.ts
+++ UI/src/features/user/services/bookingApi.ts
@@ -5,7 +5,7 @@ import type { Booking } from "../types/Booking";
 const bookingApi = {
 	getByUserId: (userId: string) => apiClient.get<ApiResponse<Booking[]>>("/bookings", { params: { entity: "user", id: userId } }).then((r) => r.data),
 	getById: (bookingId: string) => apiClient.get<ApiResponse<Booking>>("/bookings", { params: { entity: "id", id: bookingId } }).then((r) => r.data),
-	cancel: (bookingId: string) => apiClient.patch(`/bookings?id=${bookingId}`).then((r) => r.data),
+	cancel: (bookingId: string) => apiClient.patch(`/bookings/cancel?id=${bookingId}`).then((r) => r.data),
 };
 
 export default bookingApi;
