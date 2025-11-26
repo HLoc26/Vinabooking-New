1539bd4fcf8ad5950bc65d2b2eae05c9640cf217
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-26 08:12:40 +0700

    refactor(home): splitting StatsContext into seperated files

--- UI/src/main.tsx
+++ UI/src/main.tsx
@@ -7,7 +7,7 @@ import theme from "./theme/theme.ts";
 import PushNotificationProvider from "./context/PushNotification/provider.tsx";
 import AuthContextProvider from "./context/AuthContext/provider.tsx";
 import { LocationSearchProvider } from "./context/SearchContext/LocationSearchProvider.tsx";
-import { StatsProvider } from "../src/features/home/context/StatsContext.tsx";
+import StatsProvider from "../src/features/home/context/StatsContext/provider.tsx";
 import UserContextProvider from "./context/UserContext/provider.tsx";
 import ModalProvider from "./context/ModalContext/provider.tsx";
 import { BrowserRouter } from "react-router-dom";
