5f9903f92cad8a544b99052adbd8f09305a6c442
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-26 12:32:14 +0700

    feat(review): add user information in each review

--- API/review-service/src/routes/ReviewRouter.ts
+++ API/review-service/src/routes/ReviewRouter.ts
@@ -7,19 +7,21 @@ import BookingServiceClient from "../clients/BookingServiceClient";
 import ReviewRepository from "../repositories/ReviewRepository";
 import PrismaSingleton from "../clients/PrismaSingleton";
 import { AuthMiddleware } from "../middlewares/AuthMiddleware";
+import UserServiceClient from "../clients/UserServiceClient";
 
 class ReviewRouterFactory {
 	// Dependency Injection
 	public static createReviewRouter() {
 		const bookingServiceUrl = process.env["BOOKING_ENDPOINT"] ?? "http://localhost:3003";
-
+		const userServiceUrl = process.env["USER_ENDPOINT"] ?? "http://localhost:3006";
 		if (!bookingServiceUrl) {
 			throw new Error("BOOKING_ENDPOINT environment variable is not set.");
 		}
 
 		const prisma = PrismaSingleton.getInstance();
+		const userServiceClient = new UserServiceClient(userServiceUrl);
 		const reviewRepository = new ReviewRepository(prisma);
-		const reviewService = new ReviewService(reviewRepository);
+		const reviewService = new ReviewService(reviewRepository, userServiceClient);
 		const bookingServiceClient = new BookingServiceClient(bookingServiceUrl);
 		const bookingService = new BookingService(bookingServiceClient);
 		const reviewController = new ReviewController(reviewService, bookingService);
