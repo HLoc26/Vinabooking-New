959641826263557131c3c64bdb334e352547cb45
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-28 01:26:08 +0700

    feat(types): enhance Query system and add SearchResponse

--- UI/src/types/Query.ts
+++ UI/src/types/Query.ts
@@ -1,11 +1,19 @@
 import type { EAccommodationType } from "./Accommodation";
 
-export type Query = {
-	keyword: string;
+export type Guests = {
+	adults: number;
+	children: number;
+	rooms: number;
+};
+
+export type Dates = {
 	checkIn?: string | Date;
 	checkOut?: string | Date;
-	adults?: number;
-	children?: number;
-	rooms?: number;
+};
+
+export type Query = {
+	keyword: string;
+	dates?: Dates;
+	guests?: Guests;
 	type?: EAccommodationType;
 };
