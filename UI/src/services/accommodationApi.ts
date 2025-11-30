a1829684ee6c4d5f9786f45db9015c51d3a6146f
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-11-30 22:01:06 +0700

    feat(accom-api): update search params

--- UI/src/services/accommodationApi.ts
+++ UI/src/services/accommodationApi.ts
@@ -4,7 +4,7 @@ import type { AccommodationSearchData, ApiResponse } from "../types/Response";
 import apiClient from "./apiClient";
 
 const accommodationApi = {
-	search: (params: Record<string, string | number>) => apiClient.get<ApiResponse<AccommodationSearchData>>("/accommodations/search", { params }).then((r) => r.data),
+	search: (params: { [key: string]: string | number | string[] }) => apiClient.get<ApiResponse<AccommodationSearchData>>("/accommodations/search", { params }).then((r) => r.data),
 	stats: () => apiClient.get<StatsResponse>("/accommodations/stats").then((r) => r.data),
 	getByType: (type: EAccommodationType | null, page: number, limit: number = 20) =>
 		apiClient.get<ApiResponse<AccommodationSearchData>>("/accommodations/search", { params: { type: type, page, limit } }).then((r) => r.data),
