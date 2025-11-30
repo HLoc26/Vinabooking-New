67e6f8072ab2c330a150a524fefac29891e4ef34
Đặng Hữu Lộc <dhl26052004@gmail.com>
2025-12-01 02:21:19 +0700

    feat(user): implement update user information on UI

--- UI/src/context/AuthContext/provider.tsx
+++ UI/src/context/AuthContext/provider.tsx
@@ -2,8 +2,8 @@ import useAuth from "../../hooks/useAuth";
 import AuthContext from "./context";
 
 const AuthContextProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-	const { login, logout, getCurrentUser, loading, error } = useAuth();
-	return <AuthContext.Provider value={{ login, logout, getCurrentUser, loading, error }}>{children}</AuthContext.Provider>;
+	const auth = useAuth();
+	return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
 };
 
 export default AuthContextProvider;
