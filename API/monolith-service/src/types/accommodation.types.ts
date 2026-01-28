00f9c0d5c8fe132101d3daf9a3f792bcb4712f98
zhwy512 <huyngh05@gmail.com>
2026-01-28 22:35:02 +0700

    refactor(accommodation): enhance sorting functionality with ESortOption enum

--- API/monolith-service/src/types/accommodation.types.ts
+++ API/monolith-service/src/types/accommodation.types.ts
@@ -1,5 +1,13 @@
 import { Prisma, type EAccommodationType } from "@generated/client";
 
+export enum ESortOption {
+	NEWEST = "newest",
+	NAME_ASC = "name_asc",
+	NAME_DESC = "name_desc",
+	PRICE_ASC = "price_asc",
+	PRICE_DESC = "price_desc",
+}
+
 export type AccommodationWithDetails = Prisma.AccommodationGetPayload<{
 	include: {
 		address: true;
