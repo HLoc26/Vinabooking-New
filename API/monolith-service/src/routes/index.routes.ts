370eda6697d7e21d7190243ded062904f348ed5a
Đặng Hữu Lộc <dhl26052004@gmail.com>
2026-01-30 14:22:02 +0700

    feat(routers): add auth router

--- API/monolith-service/src/routes/index.routes.ts
+++ API/monolith-service/src/routes/index.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import type { Router, Request, Response } from "express";
+import AuthRouter from "./auth.routes";
 
 class AppRouter {
 	#router: Router;
 
-	constructor() {
+	constructor(private authRouter: AuthRouter) {
 		this.#router = express.Router();
 		this.#registerRoutes();
 	}
@@ -13,6 +14,7 @@ class AppRouter {
 		this.#router.get("/health", (_: Request, res: Response) => {
 			res.status(200).json({ health: "ok" });
 		});
+		this.#router.use("/auth", this.authRouter.router);
 	}
 
 	public get router(): Router {
