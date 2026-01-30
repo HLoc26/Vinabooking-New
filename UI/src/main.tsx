55f341e129c10ac0e245dca629acbeca6971189a
Đặng Hữu Lộc <dhl26052004@gmail.com>
2026-01-30 09:31:11 +0700

    feat(main): add store and queryClient provider

--- UI/src/main.tsx
+++ UI/src/main.tsx
@@ -13,27 +13,36 @@ import ModalProvider from "./context/ModalContext/provider.tsx";
 import { BrowserRouter } from "react-router-dom";
 import BookingContextProvider from "./context/BookingContext/provider.tsx";
 
+import { Provider } from "react-redux";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { store } from "./app/store.ts";
+import queryClient from "./app/queryClient.ts";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<PushNotificationProvider>
-				<AuthContextProvider>
-					<UserContextProvider>
-						<BookingContextProvider>
-							<BrowserRouter>
-								<ModalProvider>
-									<StatsProvider>
-										<SearchProvider>
-											<CssBaseline />
-											<App />
-										</SearchProvider>
-									</StatsProvider>
-								</ModalProvider>
-							</BrowserRouter>
-						</BookingContextProvider>
-					</UserContextProvider>
-				</AuthContextProvider>
-			</PushNotificationProvider>
-		</ThemeProvider>
+		<Provider store={store}>
+			<QueryClientProvider client={queryClient}>
+				<ThemeProvider theme={theme}>
+					<PushNotificationProvider>
+						<AuthContextProvider>
+							<UserContextProvider>
+								<BookingContextProvider>
+									<BrowserRouter>
+										<ModalProvider>
+											<StatsProvider>
+												<SearchProvider>
+													<CssBaseline />
+													<App />
+												</SearchProvider>
+											</StatsProvider>
+										</ModalProvider>
+									</BrowserRouter>
+								</BookingContextProvider>
+							</UserContextProvider>
+						</AuthContextProvider>
+					</PushNotificationProvider>
+				</ThemeProvider>
+			</QueryClientProvider>
+		</Provider>
 	</React.StrictMode>
 );
