07047170fafa1de7e5fba0b779b19540c50d3f2f
sang-ute <nqs28012004@gmail.com>
2026-01-30 15:32:04 +0700

    feat: add API for handling API call to backend

---
+++ UI/src/features/auth/authApi.ts
@@ -0,0 +1,11 @@
+import AxiosInstance from "../../services/apiClient";
+import { type LogInResponse, type ApiResponse } from "../../types/Response";
+
+export const login = async (payload: {
+	email: string; //
+	password: string;
+}) => {
+	const response = await AxiosInstance.post<ApiResponse<LogInResponse>>("/auth/log-in", payload).then((r) => r.data);
+	if (response.data?.accessToken && response.data?.user) return response;
+	throw new Error("Invalid login response");
+};
