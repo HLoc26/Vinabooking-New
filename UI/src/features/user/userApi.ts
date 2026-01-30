a0e771f7544f33faeee10cf9835184b012bd0453
Đặng Hữu Lộc <dhl26052004@gmail.com>
2026-01-30 09:31:37 +0700

    feat: add hook for update user info

---
+++ UI/src/features/user/userApi.ts
@@ -0,0 +1,8 @@
+import AxiosInstance from "../../services/apiClient";
+import { type ApiResponse } from "../../types/Response";
+import type { UserDto } from "../../types/UserDto";
+import type { UpdateUserInfoDto } from "./types";
+
+export const fetchUserProfileApi = () => AxiosInstance.get<ApiResponse<UserDto>>("/user/me").then((r) => r.data.data);
+
+export const updateUserProfileApi = (data: UpdateUserInfoDto) => AxiosInstance.patch<ApiResponse<UserDto>>(`/users/me`, data).then((r) => r.data.data);
