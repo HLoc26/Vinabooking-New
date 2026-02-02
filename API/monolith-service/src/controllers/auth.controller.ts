39fae565610beace023595dce2bfd239da1a6911
Đặng Hữu Lộc <dhl26052004@gmail.com>
2026-02-02 10:41:36 +0700

    feat(controller): update refresh tokien sameSite to none and increase max age

--- API/monolith-service/src/controllers/auth.controller.ts
+++ API/monolith-service/src/controllers/auth.controller.ts
@@ -112,8 +112,8 @@ class AuthController {
 		res.cookie("refresh_token", token, {
 			httpOnly: true,
 			secure: true,
-			sameSite: "strict",
-			maxAge: 7 * 24 * 60 * 60 * 1000,
+			sameSite: "none",
+			maxAge: 30 * 24 * 60 * 60 * 1000,
 		});
 	}
 
