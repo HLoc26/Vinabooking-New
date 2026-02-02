308ec1326990417cb8d23ab19c8a3a3c7627ce8e
sang-ute <nqs28012004@gmail.com>
2026-02-02 16:46:51 +0700

    feat(auth): add registration and OTP confirmation functions to authApi

--- UI/src/features/auth/authApi.ts
+++ UI/src/features/auth/authApi.ts
@@ -1,5 +1,5 @@
 import AxiosInstance from "../../services/apiClient";
-import { type LogInResponse, type ApiResponse } from "../../types/Response";
+import { type LogInResponse, type ApiResponse, type SignUpResponse, type GetOTPResponse, type ConfirmUserResponse } from "../../types/Response";
 
 export const login = async (payload: {
 	email: string; //
@@ -13,3 +13,19 @@ export const login = async (payload: {
 export const signOut = async () => {
 	return await AxiosInstance.post<ApiResponse<{ success: boolean }>>("/auth/sign-out").then((r) => r.data);
 };
+
+export const register = async (payload: {
+	name: string; //
+	email: string;
+	password: string;
+	phone: string;
+	userType: string;
+}) => AxiosInstance.post<ApiResponse<SignUpResponse>>("/auth/sign-up", payload).then((r) => r.data);
+
+export const resendOtp = async (
+	email: string //
+) => AxiosInstance.get<ApiResponse<GetOTPResponse>>(`/auth/otp?email=${email}`).then((r) => r.data);
+
+export const confirmOtp = async (payload: { username: string; confirmCode: string }) =>
+	AxiosInstance.post<ApiResponse<ConfirmUserResponse>>("/auth/sign-up/confirm", payload) //
+		.then((r) => r.data);
