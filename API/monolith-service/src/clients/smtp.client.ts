6d813f5f916a3607d1ec6aa39b011df00c38511d
zhwy512 <huyngh05@gmail.com>
2026-02-05 02:43:02 +0700

    feat(email-service): implement SMTP client and email service for sending emails

---
+++ API/monolith-service/src/clients/smtp.client.ts
@@ -0,0 +1,54 @@
+import nodemailer, { Transporter } from "nodemailer";
+import { SentMessageInfo } from "nodemailer/lib/smtp-transport";
+import { IMailClient } from "../types/email/IMailClient";
+import EnvironmentNotSetError from "../errors/EnvironmentNotSetError";
+
+class SmtpClient implements IMailClient {
+	static #instance: SmtpClient;
+
+	public static readonly user = process.env["GMAIL_APP_USER"];
+	public static readonly pass = process.env["GMAIL_APP_PASSWORD"];
+
+	private readonly transporter: Transporter;
+
+	private constructor() {
+		this.transporter = nodemailer.createTransport({
+			service: "gmail",
+			auth: {
+				user: SmtpClient.user,
+				pass: SmtpClient.pass,
+			},
+		});
+	}
+
+	public static getInstance(): SmtpClient {
+		if (!SmtpClient.user || !SmtpClient.pass) {
+			throw new EnvironmentNotSetError("Missing .env GMAIL_APP_USER or GMAIL_APP_PASSWORD");
+		}
+
+		if (!SmtpClient.#instance) {
+			SmtpClient.#instance = new SmtpClient();
+		}
+
+		return SmtpClient.#instance;
+	}
+
+	public async send(to: string[] | string, subject: string, message: string, html?: string): Promise<SentMessageInfo> {
+		try {
+			const info = await this.transporter.sendMail({
+				from: `"Vinabooking" <${SmtpClient.user}>`,
+				to,
+				subject,
+				text: message,
+				html,
+			});
+			console.log(`[SmtpClient] Email sent to: ${to} | ID: ${info.messageId}`);
+			return info;
+		} catch (err) {
+			console.error("[SmtpClient] Error sending email:", err);
+			throw err;
+		}
+	}
+}
+
+export default SmtpClient;
