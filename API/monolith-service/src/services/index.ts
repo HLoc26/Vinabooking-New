7036250f9b99211ff3dcc1084aa94b2681f529d8
sang-ute <nqs28012004@gmail.com>
2026-02-05 03:14:07 +0700

    refactor(errors, services, requests): update exports to use default exports and include AccommodationService

--- API/monolith-service/src/services/index.ts
+++ API/monolith-service/src/services/index.ts
@@ -1,3 +1,4 @@
 export { default as OAuthService } from "./oauth.service";
 export { default as AuthService } from "./auth.service";
 export { default as UserService } from "./user.service";
+export { default as AccommodationService } from "./accommodation.service";
--- API/monolith-service/src/types/requests/index.ts
+++ API/monolith-service/src/types/requests/index.ts
@@ -1,3 +1,4 @@
 export * from "./auth.request";
 export * from "./user.requests";
 export * from "./image.request";
+export * from "./accommodation.requests";
