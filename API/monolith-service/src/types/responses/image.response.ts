039f9bf1035b657af6c55a144b2a569c19d66304
Đặng Hữu Lộc <dhl26052004@gmail.com>
2026-02-07 00:38:01 +0700

    feat(types): update image full infor type and remove redundants
    refactor(type): remove redundant types

--- API/monolith-service/src/types/responses/image.response.ts
+++ API/monolith-service/src/types/responses/image.response.ts
@@ -1,10 +1,6 @@
-import { ResponseImage, UploadedImage } from "@/types/image.types";
+import { UploadedImage } from "@/types/image.types";
 
 export interface UploadResponse {
 	success: boolean;
 	images: UploadedImage[];
-}
-
-export interface GetImageResponse {
-	images: ResponseImage[];
 }
