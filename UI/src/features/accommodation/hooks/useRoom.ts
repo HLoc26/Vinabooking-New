dec01da78f0251cdbc30afcb4839156eb2434c5d
sang-ute <nqs28012004@gmail.com>
2026-02-09 23:54:24 +0700

    feat(accommodation): add useRoom hook to fetch room data by accommodation ID

---
+++ UI/src/features/accommodation/hooks/useRoom.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { getRoomByAccommodationId } from "../roomApi";
+
+/**
+ * To fetch room data by ID
+ */
+const useRoom = (accommodationId: string, startDate: Date, endDate: Date) => {
+	return useQuery({
+		queryKey: ["room", accommodationId],
+		queryFn: async () => {
+			const response = await getRoomByAccommodationId(accommodationId, startDate, endDate);
+			if (!response) throw new Error("No data found");
+			const data = response.data;
+			if (!data) throw new Error(response.error as string);
+
+			return data;
+		},
+	});
+};
+export default useRoom;
