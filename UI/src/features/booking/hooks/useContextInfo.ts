e97f595d0ca0226abf843e27d6bb3de28a20990f
sang-ute <nqs28012004@gmail.com>
2026-02-20 00:07:35 +0700

    delete(booking): delete all the unnecessary files and contexts that are not used in Booking feature

--- UI/src/features/booking/hooks/useContextInfo.ts
+++
@@ -1,8 +0,0 @@
-import type { ContextInfoAdapter } from "../context/ContextInfoAdapter";
-
-/**
- * Returns the provided ContextInfo.
- */
-export function useContextInfo(info: ContextInfoAdapter): ContextInfoAdapter {
-	return info;
-}
