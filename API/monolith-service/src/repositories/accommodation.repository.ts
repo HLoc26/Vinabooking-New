8f51ecba853bd0e89140e256d090721656cd7c6d
Đặng Hữu Lộc <dhl26052004@gmail.com>
2026-02-21 01:38:59 +0700

    refactor(accommodation): optimize search logic and enhance data retrieval for accommodations
    x
    
    c

--- API/monolith-service/src/repositories/accommodation.repository.ts
+++ API/monolith-service/src/repositories/accommodation.repository.ts
@@ -1,5 +1,5 @@
 import { PrismaClient, Prisma, type EAccommodationType } from "@/generated/client";
-import { SearchFilters, AccommodationWithDetails, AccommodationSearchResult, ESortOption } from "@/types/accommodation.types";
+import { SearchFilters, AccommodationWithDetails, AccommodationSearchResult, ESortOption, AccommodationFullInfo } from "@/types/accommodation.types";
 
 class AccommodationRepository {
 	readonly #prismaClient: PrismaClient;
@@ -85,28 +85,80 @@ class AccommodationRepository {
 			}));
 		}
 
-		// 5. Sort
-		let orderBy: Prisma.AccommodationOrderByWithRelationInput = { createdAt: Prisma.SortOrder.desc };
+		// 1. Pre-count result
+		const total = await this.#prismaClient.accommodation.count({ where });
+		if (total === 0) return { data: [], total: 0 };
 
-		switch (sortBy) {
-			case ESortOption.NAME_ASC:
-				orderBy = { name: Prisma.SortOrder.asc };
-				break;
-			case ESortOption.NAME_DESC:
-				orderBy = { name: Prisma.SortOrder.desc };
-				break;
-		}
+		// 2. Get all IDs that matches
+		const matchingRecords = await this.#prismaClient.accommodation.findMany({
+			where,
+			select: { id: true },
+		});
+		const matchedIds = matchingRecords.map((r) => r.id);
 
-		const [data, total] = await Promise.all([
-			this.#prismaClient.accommodation.findMany({
-				where,
-				include: { address: true, facilities: { include: { facility: true } } },
-				skip: offset,
-				take: limit,
-				orderBy,
-			}),
-			this.#prismaClient.accommodation.count({ where }),
-		]);
+		// TODO: Add minPrice, avgStar, and reviewCount column for faster query
+		// 3. Use raw SQL
+		let orderClause = Prisma.sql`ORDER BY a.createdAt DESC`;
+		if (sortBy === ESortOption.RECOMMENDED) orderClause = Prisma.sql`ORDER BY minPrice IS NULL, avgStar IS NULL, minPrice ASC, avgStar DESC`;
+		if (sortBy === ESortOption.PRICE_ASC) orderClause = Prisma.sql`ORDER BY minPrice IS NULL, minPrice ASC`;
+		if (sortBy === ESortOption.PRICE_DESC) orderClause = Prisma.sql`ORDER BY minPrice IS NULL, minPrice DESC`;
+		if (sortBy === ESortOption.NAME_ASC) orderClause = Prisma.sql`ORDER BY a.name ASC`;
+		if (sortBy === ESortOption.NAME_DESC) orderClause = Prisma.sql`ORDER BY a.name DESC`;
+		if (sortBy === ESortOption.RATING) orderClause = Prisma.sql`ORDER BY avgStar IS NULL, avgStar DESC`;
+
+		// TODO: unify table names in prisma.schema
+		const roomTable = "rooms";
+		const accommodationTable = "accommodations";
+		const reviewTable = "Review";
+		const bedTable = "beds";
+		const statsRows = await this.#prismaClient.$queryRaw<{ id: string; minPrice: number | null; avgStar: number | null; reviewCount: number }[]>`
+            SELECT 
+                a.id,
+                a.name,
+                a.createdAt,
+                (
+                    SELECT MIN(
+                        COALESCE(
+                            CAST(NULLIF(r.price, '') AS DECIMAL(10,2)),
+                            (SELECT MIN(CAST(b.price AS DECIMAL(10,2))) FROM ${Prisma.raw(bedTable)} b WHERE b.roomId = r.id)
+                        )
+                    )
+					FROM ${Prisma.raw(roomTable)} r
+                    WHERE r.accommodationId = a.id
+                ) AS minPrice,
+                (
+                    SELECT AVG(rev.star) FROM ${Prisma.raw(reviewTable)} rev WHERE rev.accommodationId = a.id
+                ) AS avgStar,
+                (
+                    SELECT COUNT(rev.id) FROM ${Prisma.raw(reviewTable)} rev WHERE rev.accommodationId = a.id
+                ) AS reviewCount
+			FROM ${Prisma.raw(accommodationTable)} a
+            WHERE a.id IN (${Prisma.join(matchedIds)})
+            ${orderClause}
+            LIMIT ${limit} OFFSET ${offset}
+        `;
+
+		const paginatedIds = statsRows.map((row) => row.id);
+		if (paginatedIds.length === 0) return { data: [], total };
+
+		// 4. Fetch detail for paginated record
+		const unsortedData = await this.#prismaClient.accommodation.findMany({
+			where: { id: { in: paginatedIds } },
+			include: { address: true, facilities: { include: { facility: true } } },
+		});
+
+		// 5. Merge data, keep raw SQL's order
+		const data = paginatedIds.map((id) => {
+			const acc = unsortedData.find((d) => d.id === id)!;
+			const stats = statsRows.find((s) => s.id === id)!;
+
+			return {
+				...acc,
+				minPrice: stats.minPrice ? Number(stats.minPrice) : undefined,
+				avgStar: stats.avgStar ? Number(stats.avgStar) : null,
+				reviewCount: Number(stats.reviewCount || 0),
+			} as unknown as AccommodationFullInfo;
+		});
 
 		return { data, total };
 	}
