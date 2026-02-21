a793bc74d337602a337afe60283a478fa8cc91ad
sang-ute <nqs28012004@gmail.com>
2026-02-21 17:00:05 +0700

    feat(booking): add dispatch as well as removing context provider for applying redux inside the room tab

--- UI/src/features/accommodation/components/detail/tabs/Rooms/RoomsTab.tsx
+++ UI/src/features/accommodation/components/detail/tabs/Rooms/RoomsTab.tsx
@@ -1,22 +1,32 @@
 import { Box } from "@mui/material";
 import { RoomCard } from "./components/RoomCard";
-import useBookingContextProvider from "../../../../../../context/BookingContext/hook";
 import useAccommodationRooms from "../../../../hooks/useAccommodationRooms";
+
+import { useSelector, useDispatch } from "react-redux";
+import type { RootState, AppDispatch } from "../../../../../../app/store";
+import { updateRoomQuantity } from "../../../../../../features/booking/bookingSlice";
 
 interface Props {
 	accommodationId: string;
 }
 
 export const RoomsTab = ({ accommodationId }: Props) => {
-	const { bookingInfo, updateRoomQuantity } = useBookingContextProvider();
+	const dispatch = useDispatch<AppDispatch>();
+	const bookingInfo = useSelector((state: RootState) => state.booking);
+
 	const { data: rooms } = useAccommodationRooms(accommodationId, bookingInfo.startDate, bookingInfo.endDate);
+
 	if (!rooms) return null;
+
 	return (
 		<Box>
 			{rooms.map((room) => {
 				const selectedRoom = bookingInfo.items.find((r) => r.id === room.id);
+
 				const selectedQuantity = selectedRoom ? selectedRoom.count : 0;
+
 				const availableRooms = room.remainingQuantity ?? 0;
+
 				return (
 					<RoomCard
 						key={room.id}
@@ -25,10 +35,22 @@ export const RoomsTab = ({ accommodationId }: Props) => {
 						availableRooms={availableRooms}
 						onIncrease={() => {
 							if (selectedQuantity < availableRooms) {
-								updateRoomQuantity(room.id, selectedQuantity + 1);
+								dispatch(
+									updateRoomQuantity({
+										roomId: room.id,
+										count: selectedQuantity + 1,
+									})
+								);
 							}
 						}}
-						onDecrease={() => updateRoomQuantity(room.id, selectedQuantity - 1)}
+						onDecrease={() =>
+							dispatch(
+								updateRoomQuantity({
+									roomId: room.id,
+									count: selectedQuantity - 1,
+								})
+							)
+						}
 					/>
 				);
 			})}
