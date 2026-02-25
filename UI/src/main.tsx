6fc69a70d41af8d966451e5a2cfea374e2d9c7a4
zhwy512 <huyngh05@gmail.com>
2026-02-25 11:15:07 +0700

    refactor(main.tsx): consolidate store and persistor imports

--- UI/src/main.tsx
+++ UI/src/main.tsx
@@ -9,11 +9,10 @@ import StatsProvider from "../src/features/home/context/StatsContext/provider.ts
 import ModalProvider from "./context/ModalContext/provider.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
-import { persistor } from "./app/store.ts";
+import { store, persistor } from "./app/store.ts";
 
 import { Provider } from "react-redux";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { store } from "./app/store.ts";
 import queryClient from "./app/queryClient.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
