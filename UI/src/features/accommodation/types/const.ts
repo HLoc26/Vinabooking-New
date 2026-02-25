72605fd4f9c8e11a56e481bf613ee40cc3644c05
sang-ute <nqs28012004@gmail.com>
2026-02-25 22:21:09 +0700

    refactor(images): simplify default image assignments in ACCOMMODATION_DEFAULT_IMAGES

--- UI/src/features/accommodation/types/const.ts
+++ UI/src/features/accommodation/types/const.ts
@@ -1,47 +1,25 @@
 import { EAccommodationType } from "../types/accommodation.types";
 
-import allImg from "../../../images/ALL.jpg";
-import hotelImg from "../../../images/HOTEL.avif";
-import apartmentImg from "../../../images/APARTMENT.avif";
-import villaImg from "../../../images/VILLA.avif";
-import vacationHomeImg from "../../../images/VACATION_HOME.avif";
-import guesthouseImg from "../../../images/GUESTHOUSE.avif";
-import hostelImg from "../../../images/HOSTEL.avif";
-import bedAndBreakfastImg from "../../../images/BED_AND_BREAKFAST.jpg";
-import homestayImg from "../../../images/HOMESTAY.avif";
-import campgroundImg from "../../../images/CAMPGROUND.avif";
-import countryHouseImg from "../../../images/COUNTRY_HOUSE.avif";
-import boatImg from "../../../images/BOAT.avif";
-import luxuryTentImg from "../../../images/LUXURY_TENT.avif";
-import cabinImg from "../../../images/CABIN.avif";
-import motelImg from "../../../images/MOTEL.avif";
-import resortImg from "../../../images/RESORT.avif";
-import farmstayImg from "../../../images/FARMSTAY.avif";
-import capsuleHotelImg from "../../../images/CAPSULE_HOTEL.avif";
-import treehouseImg from "../../../images/TREEHOUSE.avif";
-import townhouseImg from "../../../images/TOWNHOUSE.avif";
-import otherImg from "../../../images/OTHER.avif";
-
 export const ACCOMMODATION_DEFAULT_IMAGES: Partial<Record<EAccommodationType, string>> = {
-	[EAccommodationType.ALL]: allImg,
-	[EAccommodationType.HOTEL]: hotelImg,
-	[EAccommodationType.APARTMENT]: apartmentImg,
-	[EAccommodationType.VILLA]: villaImg,
-	[EAccommodationType.VACATION_HOME]: vacationHomeImg,
-	[EAccommodationType.GUESTHOUSE]: guesthouseImg,
-	[EAccommodationType.HOSTEL]: hostelImg,
-	[EAccommodationType.BED_AND_BREAKFAST]: bedAndBreakfastImg,
-	[EAccommodationType.HOMESTAY]: homestayImg,
-	[EAccommodationType.CAMPGROUND]: campgroundImg,
-	[EAccommodationType.COUNTRY_HOUSE]: countryHouseImg,
-	[EAccommodationType.BOAT]: boatImg,
-	[EAccommodationType.LUXURY_TENT]: luxuryTentImg,
-	[EAccommodationType.CABIN]: cabinImg,
-	[EAccommodationType.MOTEL]: motelImg,
-	[EAccommodationType.RESORT]: resortImg,
-	[EAccommodationType.FARMSTAY]: farmstayImg,
-	[EAccommodationType.CAPSULE_HOTEL]: capsuleHotelImg,
-	[EAccommodationType.TREEHOUSE]: treehouseImg,
-	[EAccommodationType.TOWNHOUSE]: townhouseImg,
-	[EAccommodationType.OTHER]: otherImg,
+	[EAccommodationType.ALL]: "/images/ALL.jpg",
+	[EAccommodationType.HOTEL]: "/images/HOTEL.avif",
+	[EAccommodationType.APARTMENT]: "/images/APARTMENT.avif",
+	[EAccommodationType.VILLA]: "/images/VILLA.avif",
+	[EAccommodationType.VACATION_HOME]: "/images/VACATION_HOME.avif",
+	[EAccommodationType.GUESTHOUSE]: "/images/GUESTHOUSE.avif",
+	[EAccommodationType.HOSTEL]: "/images/HOSTEL.avif",
+	[EAccommodationType.BED_AND_BREAKFAST]: "/images/BED_AND_BREAKFAST.jpg",
+	[EAccommodationType.HOMESTAY]: "/images/HOMESTAY.avif",
+	[EAccommodationType.CAMPGROUND]: "/images/CAMPGROUND.avif",
+	[EAccommodationType.COUNTRY_HOUSE]: "/images/COUNTRY_HOUSE.avif",
+	[EAccommodationType.BOAT]: "/images/BOAT.avif",
+	[EAccommodationType.LUXURY_TENT]: "/images/LUXURY_TENT.avif",
+	[EAccommodationType.CABIN]: "/images/CABIN.avif",
+	[EAccommodationType.MOTEL]: "/images/MOTEL.avif",
+	[EAccommodationType.RESORT]: "/images/RESORT.avif",
+	[EAccommodationType.FARMSTAY]: "/images/FARMSTAY.avif",
+	[EAccommodationType.CAPSULE_HOTEL]: "/images/CAPSULE_HOTEL.avif",
+	[EAccommodationType.TREEHOUSE]: "/images/TREEHOUSE.avif",
+	[EAccommodationType.TOWNHOUSE]: "/images/TOWNHOUSE.avif",
+	[EAccommodationType.OTHER]: "/images/OTHER.avif",
 };
