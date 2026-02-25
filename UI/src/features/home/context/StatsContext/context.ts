3186ba761260dd19d2044663c36778f5418b36d2
Đặng Hữu Lộc <dhl26052004@gmail.com>
2026-02-25 16:10:38 +0700

    refactor(HomeContent): replace StatsContext with useAccommodationStats hook and remove unused StatsContext files

--- UI/src/features/home/context/StatsContext/context.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import type { StatsContextValue } from "../../types/Stats";
-
-const StatsContext = createContext<StatsContextValue | null>(null);
-
-export default StatsContext;
