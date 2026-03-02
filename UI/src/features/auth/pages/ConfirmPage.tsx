c80617cdde01c43be6747b28e7e4aa8273d33b67
Đặng Hữu Lộc <dhl26052004@gmail.com>
2026-03-02 21:46:23 +0700

    feat(auth): add authentication pages and update routing structure

--- UI/src/features/auth/pages/ConfirmPage.tsx
+++ UI/src/features/auth/pages/ConfirmPage.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { Box, Button, Typography, Link } from "@mui/material";
 import { useLocation, useNavigate } from "react-router-dom";
-import type { OtpLocationState } from "../features/auth/types/LocationState";
-import { useResendOtp, useConfirmOtp } from "../features/auth/hooks/useRegister";
-import OtpInput from "../components/shared/OtpInput";
-import { usePushNotificationContext } from "../context/PushNotification/hook";
+import type { OtpLocationState } from "../types/LocationState";
+import { useResendOtp, useConfirmOtp } from "../hooks/useRegister";
+import OtpInput from "../../../components/shared/OtpInput";
+import { usePushNotificationContext } from "../../../context/PushNotification/hook";
 
 const ConfirmOTPPage: React.FC = () => {
 	const navigate = useNavigate();
