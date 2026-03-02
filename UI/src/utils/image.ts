aefd0924e9c4967be5d074068c3699524022529e
sang-ute <nqs28012004@gmail.com>
2026-03-02 20:41:26 +0700

    feat(review): add getThumbnailUrls to handle multiple images in one review

--- UI/src/utils/image.ts
+++ UI/src/utils/image.ts
@@ -6,3 +6,11 @@ export const getThumbnailUrl = (imgs: Image[]): string => {
 	const displayUrl = thumbnailVariant?.url ?? targetImage?.url;
 	return displayUrl;
 };
+export const getThumbnailUrls = (imgs: Image[]): string[] => {
+	if (!imgs) return [];
+
+	return imgs.map((img) => {
+		const thumb = img.variants.find((v) => v.variant === "THUMBNAIL");
+		return thumb?.url ?? img.url;
+	});
+};
