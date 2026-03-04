5aafddf1594c477e2cd0f8d99cc3cd84eaea1534
Đặng Hữu Lộc <dhl26052004@gmail.com>
2026-03-04 15:14:42 +0700

    feat(register): add type check to navigate to different otp url in RegisterForm

--- UI/src/routes/owner.tsx
+++ UI/src/routes/owner.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import OwnerLandingPage from "../features/home/pages/OwnerLandingPage";
 import RegisterPage from "../features/auth/pages/RegisterPage";
+import ConfirmOTPPage from "../features/auth/pages/ConfirmOTPPage";
 
 export const OwnerRouter = () => (
 	<Routes>
 		<Route path="/landing" element={<OwnerLandingPage />} />
 		<Route path="/register" element={<RegisterPage />} />
+		<Route path="/otp" element={<ConfirmOTPPage />} />
 	</Routes>
 );
