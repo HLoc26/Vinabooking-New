1dc75de14e749c13a35ce57262a6c25ac20c8658
Đặng Hữu Lộc <dhl26052004@gmail.com>
2026-03-04 15:21:10 +0700

    feat(route): add owner login route

--- UI/src/routes/owner.tsx
+++ UI/src/routes/owner.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import OwnerLandingPage from "../features/home/pages/OwnerLandingPage";
 import RegisterPage from "../features/auth/pages/RegisterPage";
 import ConfirmOTPPage from "../features/auth/pages/ConfirmOTPPage";
+import LoginPage from "../features/auth/pages/LoginPage";
 
 export const OwnerRouter = () => (
 	<Routes>
 		<Route path="/landing" element={<OwnerLandingPage />} />
 		<Route path="/register" element={<RegisterPage />} />
+		<Route path="/login" element={<LoginPage />} />
 		<Route path="/otp" element={<ConfirmOTPPage />} />
 	</Routes>
 );
