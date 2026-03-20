1f83a5d978805336d9caf7cfc88131309b657b7a
Đặng Hữu Lộc <dhl26052004@gmail.com>
2026-03-21 00:22:05 +0700

    feat: add UnderConstruction component and sidebar for owner page

---
+++ UI/src/features/owner/pages/ManageBookingPage.tsx
@@ -0,0 +1,7 @@
+import UnderConstruction from "../../../components/shared/UnderConstruction";
+
+const ManageBookingPage = () => {
+  return <UnderConstruction />;
+};
+
+export default ManageBookingPage;
---
+++ UI/src/features/owner/pages/ManagePricePage.tsx
@@ -0,0 +1,7 @@
+import UnderConstruction from "../../../components/shared/UnderConstruction";
+
+const ManagePricePage = () => {
+  return <UnderConstruction />;
+};
+
+export default ManagePricePage;
