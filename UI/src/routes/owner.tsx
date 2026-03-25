3359b4e2a61500c0915c14cb6fb1512c473b61ea
zhwy512 <huyngh05@gmail.com>
2026-03-25 20:46:24 +0700

    fix(ui/owner): remove unnecessary route

--- UI/src/routes/owner.tsx
+++ UI/src/routes/owner.tsx
@@ -3,7 +3,6 @@ import OwnerLandingPage from "../features/home/pages/OwnerLandingPage";
 import RegisterPage from "../features/auth/pages/RegisterPage";
 import ConfirmOTPPage from "../features/auth/pages/ConfirmOTPPage";
 import LoginPage from "../features/auth/pages/LoginPage";
-import OwnerHomePage from "../features/owner/pages/OwnerHomePage";
 import { OwnerLayout } from "../components/layout/OwnerLayout";
 import OwnerOnboardPage from "../features/owner/pages/OwnerOnboardPage";
 import DashboardPage from "../features/owner/pages/DashboardPage";
