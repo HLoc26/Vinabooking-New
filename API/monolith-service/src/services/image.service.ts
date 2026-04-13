aca8255e56478632604d38cd04c7177dfcb320b1
Đặng Hữu Lộc <dhl26052004@gmail.com>
2026-04-13 20:07:16 +0700

    feat(image): implement deleteImage service logic

--- API/monolith-service/src/services/image.service.ts
+++ API/monolith-service/src/services/image.service.ts
@@ -26,6 +26,13 @@ class ImageService {
 		return this._sanitizeImage(images);
 	}
 
+	async deleteImage(id: string) {
+		const keys = await this.#imageRepository.deleteImage(id);
+		if (keys.length > 0) {
+			await this.#s3Service.deleteFiles(keys);
+		}
+	}
+
 	async deleteImagesByEntity(type: EEntityType, id: string) {
 		const keys = await this.#imageRepository.deleteEntityImages(type, id);
 		if (keys.length > 0) {
