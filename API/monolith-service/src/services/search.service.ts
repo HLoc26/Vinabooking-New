a814dc0a3b67dc097712ab7c3d164f1fc53c0d8a
Đặng Hữu Lộc <dhl26052004@gmail.com>
2026-05-02 20:48:23 +0700

    feat: add caching for semantic search

--- API/monolith-service/src/services/search.service.ts
+++ API/monolith-service/src/services/search.service.ts
@@ -1,16 +1,35 @@
 import { getEmbeddingModel } from "@/clients/gemini.client";
 import { pineconeIndex, UnifiedRecordMetadata } from "@/clients/pinecone.client";
+import { redisClient } from "@/registry";
 import { AccommodationMatchStats } from "@/types/search.types";
 import { aiLimiter } from "@/utils/ai-limiter";
 import { QueryResponse, ScoredPineconeRecord } from "@pinecone-database/pinecone";
+
+import crypto from "crypto";
 
 /**
  * Service for semantic search and AI stuff
  */
 class SearchService {
-	// TODO: save hash query to redis for search caching
-	async semanticSearch(query: string, location: string) {
+	async semanticSearch(query: string, location: string): Promise<AccommodationMatchStats[]> {
 		try {
+			if (!query || query.trim() === "") {
+				return [];
+			}
+
+			const queryHash = this.#createQueryHash(query, location);
+
+			const cacheKey = `semantic_search:${queryHash}`;
+
+			const cachedResults = await redisClient.GET(cacheKey);
+
+			if (cachedResults) {
+				console.log(`[CACHE HIT] Semantic Search: ${query} in ${location}`);
+				return JSON.parse(cachedResults) as AccommodationMatchStats[];
+			}
+
+			console.log(`[CACHE MISS] Executing full pipeline for: ${query} in ${location}`);
+
 			const vector = await this.#vectorizeIntent(query);
 
 			const records = await this.#queryVectorDb(vector, location);
@@ -20,10 +39,40 @@ class SearchService {
 			// Search by finalScore
 			matchStats.sort((a, b) => b.finalScore - a.finalScore);
 
+			await redisClient.SET(cacheKey, JSON.stringify(matchStats), {
+				EX: 3600,
+			});
+
 			return matchStats;
 		} catch (error) {
 			console.error(`[Semantic Search] Error when search with query: ${query}\nLocation: ${location}`);
+			return [];
 		}
+	}
+
+	/**
+	 * Create unique hash for query:location pair
+	 */
+	#createQueryHash(query: string, location: string): string {
+		// Hàm helper để chuẩn hóa chuỗi
+		const sanitize = (str: string) => {
+			if (!str) return "";
+			return (
+				str
+					.replace(/[^\p{L}\p{N}\s]/gu, "")
+					// remove consecutive spaces
+					.replace(/\s+/g, " ")
+					.trim()
+					.toLowerCase()
+			);
+		};
+
+		const normalizedQuery = sanitize(query);
+		const normalizedLocation = sanitize(location);
+
+		const rawString = `${normalizedLocation}::${normalizedQuery}`;
+
+		return crypto.createHash("sha256").update(rawString).digest("hex");
 	}
 
 	#aggregateScores(matches: ScoredPineconeRecord<UnifiedRecordMetadata>[]): AccommodationMatchStats[] {
