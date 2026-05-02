7b6a86a42ab99c619f5851a7385c9ff82663d8e9
Đặng Hữu Lộc <dhl26052004@gmail.com>
2026-05-02 20:20:49 +0700

    feat(search): update pinecone client with metadata types

--- API/monolith-service/src/clients/pinecone.client.ts
+++ API/monolith-service/src/clients/pinecone.client.ts
@@ -1,9 +1,20 @@
-import { Pinecone } from "@pinecone-database/pinecone";
+import { Pinecone, RecordMetadata } from "@pinecone-database/pinecone";
 
 const pc = new Pinecone({
 	apiKey: process.env["PINECONE_API_KEY"]!,
 });
 
-export const pineconeIndex = pc.index({ name: process.env["PINECONE_INDEX_NAME"]! });
+export interface ReviewRecordMetadata extends RecordMetadata {
+	reviewId: string;
+	text: string;
+	accommodationId: string;
+	type: "accommodation-profile" | "review";
+	city: string;
+	rating: number;
+	sentiment: string;
+	createdAt: number;
+}
+
+export const pineconeIndex = pc.index<ReviewRecordMetadata>({ name: process.env["PINECONE_INDEX_NAME"]! });
 
 export default pc;
